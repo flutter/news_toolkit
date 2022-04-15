67fd7105711dcbe5dacf3ef67e550175b51984db
Bart Selwesiuk <bartosz@verygood.ventures>
2022-04-15 18:55:45 +0200

    feat: navigate to home for authenticated and unauthenticated users

--- lib/app/routes/routes.dart
+++ lib/app/routes/routes.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:google_news_template/app/app.dart';
 import 'package:google_news_template/home/home.dart';
-import 'package:google_news_template/login/login.dart';
 import 'package:google_news_template/onboarding/onboarding.dart';
 
 List<Page> onGenerateAppViewPages(AppStatus state, List<Page<dynamic>> pages) {
@@ -9,7 +8,6 @@ List<Page> onGenerateAppViewPages(AppStatus state, List<Page<dynamic>> pages) {
     case AppStatus.onboardingRequired:
       return [OnboardingPage.page()];
     case AppStatus.unauthenticated:
-      return [LoginPage.page()];
     case AppStatus.authenticated:
       return [HomePage.page()];
   }
