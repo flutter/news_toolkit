dedecb0d295f0aeb41e79f25a15de3ccae637cf8
Bart Selwesiuk <bartosz@verygood.ventures>
2022-04-20 14:01:42 +0200

    style: use AppSpacing in NavigationDrawer

--- lib/navigation/view/navigation_drawer.dart
+++ lib/navigation/view/navigation_drawer.dart
@@ -4,23 +4,23 @@ import 'package:flutter/material.dart';
 class NavigationDrawer extends StatelessWidget {
   const NavigationDrawer({Key? key}) : super(key: key);
 
-  static const _contentPadding = 16.0;
+  static const _contentPadding = AppSpacing.lg;
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: const BorderRadius.only(
-        topRight: Radius.circular(16),
-        bottomRight: Radius.circular(16),
+        topRight: Radius.circular(AppSpacing.lg),
+        bottomRight: Radius.circular(AppSpacing.lg),
       ),
       child: Drawer(
         backgroundColor: AppColors.darkBackground,
         child: ScrollableColumn(
           padding: const EdgeInsets.only(
             top: kToolbarHeight,
-            left: 16,
-            right: 16,
-            bottom: 16,
+            left: AppSpacing.lg,
+            right: AppSpacing.lg,
+            bottom: AppSpacing.lg,
           ),
           children: [
             Padding(
