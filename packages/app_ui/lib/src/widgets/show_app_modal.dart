6aec8314ae4328abd1ab1757cc2d5e09c27848a1
Ana Polo <anna-91_@hotmail.com>
2022-04-21 13:07:45 +0200

    chore: added show app modal class

---
+++ packages/app_ui/lib/src/widgets/show_app_modal.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+/// Modal which is styled for the Google news template app.
+Future<T?> showAppModal<T>({
+  required BuildContext context,
+  required WidgetBuilder builder,
+  Color? backgroundColor,
+  double? elevation,
+  Clip? clipBehavior,
+  BoxConstraints? constraints,
+  Color? barrierColor,
+  bool useRootNavigator = false,
+  bool isDismissible = true,
+  bool enableDrag = true,
+  RouteSettings? routeSettings,
+  AnimationController? transitionAnimationController,
+}) {
+  return showModalBottomSheet(
+    context: context,
+    builder: builder,
+    isScrollControlled: true,
+    backgroundColor: backgroundColor,
+    barrierColor: barrierColor,
+    isDismissible: isDismissible,
+    enableDrag: enableDrag,
+    routeSettings: routeSettings,
+    transitionAnimationController: transitionAnimationController,
+    elevation: elevation,
+    constraints: constraints,
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(16),
+        topRight: Radius.circular(16),
+      ),
+    ),
+    clipBehavior: Clip.hardEdge,
+  );
+}
--- packages/app_ui/lib/src/widgets/widgets.dart
+++ packages/app_ui/lib/src/widgets/widgets.dart
@@ -1,3 +1,4 @@
 export 'app_divider.dart';
 export 'app_logo.dart';
 export 'scrollable_column.dart';
+export 'show_app_modal.dart';
