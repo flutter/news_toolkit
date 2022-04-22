9de1ef4360487d3b9a1aee384abfcf6adfd37b01
Ana Polo <anna-91_@hotmail.com>
2022-04-22 16:26:24 +0200

    fix: deleted unnecessary import

--- packages/app_ui/test/src/widgets/app_logo_test.dart
+++ packages/app_ui/test/src/widgets/app_logo_test.dart
@@ -1,5 +1,4 @@
 import 'package:app_ui/app_ui.dart';
-import 'package:app_ui/src/generated/assets.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
