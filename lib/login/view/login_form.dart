7e3da17c9229d023c0b62734c0f56d73962e967b
Ana Polo <anna-91_@hotmail.com>
2022-04-26 11:42:31 +0200

    fix: deleted unused import

--- lib/login/view/login_form.dart
+++ lib/login/view/login_form.dart
@@ -1,5 +1,5 @@
 import 'package:app_ui/app_ui.dart'
-    show AppButton, AppSpacing, AppTextStyle, AppColors;
+    show AppButton, AppSpacing, AppTextStyle;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:formz/formz.dart';
