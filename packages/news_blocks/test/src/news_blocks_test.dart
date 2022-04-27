f72b35a065a824865b45c273f9dd82190ee7f01a
Felix Angelov <felangelov@gmail.com>
2022-04-27 16:41:55 -0500

    chore(news_blocks): create news blocks package (#47)

---
+++ packages/news_blocks/test/src/news_blocks_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:news_blocks/news_blocks.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('NewsBlocks', () {
+    test('can be instantiated', () {
+      expect(NewsBlocks(), isNotNull);
+    });
+  });
+}
