ed6153e46a4a48067432053cd880054d5faddf94
Felix Angelov <felangelov@gmail.com>
2022-04-27 11:25:53 -0500

    refactor(api): add controller abstraction

--- api/bin/server.dart
+++ api/bin/server.dart
@@ -1,13 +1,14 @@
 import 'dart:io';
 
+import 'package:google_news_template_api/google_news_template_api.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
-import 'package:shelf_router/shelf_router.dart';
 
 Future<void> main() async {
-  final router = Router()..get('/', (Request _) => Response(204));
-  final handler =
-      const Pipeline().addMiddleware(logRequests()).addHandler(router);
+  const controller = ApiController();
+  final handler = const Pipeline()
+      .addMiddleware(logRequests())
+      .addHandler(controller.handler);
   final ip = InternetAddress.anyIPv4;
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
   final server = await serve(handler, ip, port);
