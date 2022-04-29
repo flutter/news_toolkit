3aca24a4aeb861a47ce7ce0ce6b03b26442a3310
Bart Selwesiuk <bartosz@verygood.ventures>
2022-04-29 13:03:38 +0200

    test(app_ui): update text style tests

--- packages/app_ui/test/src/helpers/pump_app.dart
+++ packages/app_ui/test/src/helpers/pump_app.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 extension AppTester on WidgetTester {
-  Future<void> pumpApp(Widget widgetUnderTest) async {
+  Future<void> pumpApp(
+    Widget widgetUnderTest, {
+    ThemeData? theme,
+  }) async {
     await pumpWidget(
       MaterialApp(
+        theme: theme,
         home: Scaffold(
           body: widgetUnderTest,
         ),
