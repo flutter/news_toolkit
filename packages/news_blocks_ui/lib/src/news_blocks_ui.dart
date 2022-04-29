55d8e469da21d96942b4b40282e0d672a6293c2b
Felix Angelov <felangelov@gmail.com>
2022-04-29 16:34:48 -0500

    chore(news_blocks_ui): create package:news_blocks_ui

---
+++ packages/news_blocks_ui/lib/src/news_blocks_ui.dart
@@ -0,0 +1,7 @@
+/// {@template news_blocks_ui}
+/// A Flutter package that contains UI Components for news blocks.
+/// {@endtemplate}
+class NewsBlocksUi {
+  /// {@macro news_blocks_ui}
+  const NewsBlocksUi();
+}
