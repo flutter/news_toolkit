5ed8b44875645463f59d13d89b5a3a9d25185495
Felix Angelov <felangelov@gmail.com>
2022-04-29 17:28:55 -0500

    feat(api): add GoogleNewsTemplateApiClient w/getFeed

--- api/lib/client.dart
+++ api/lib/client.dart
@@ -4,4 +4,9 @@ library client;
 export 'src/api/v1/categories/get_categories/models/models.dart'
     show CategoriesResponse;
 export 'src/api/v1/feed/get_feed/models/models.dart' show FeedResponse;
+export 'src/client/google_news_template_api_client.dart'
+    show
+        GoogleNewsTemplateApiClient,
+        GoogleNewsTemplateApiMalformedResponse,
+        GoogleNewsTemplateApiRequestFailure;
 export 'src/data/models/models.dart' show Category, Feed, NewsBlocksConverter;
