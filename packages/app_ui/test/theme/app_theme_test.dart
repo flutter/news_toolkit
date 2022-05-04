117a8c1a107163a2cb2b9d52731d7f67ae38d1f0
Bart Selwesiuk <bartosz@verygood.ventures>
2022-05-04 18:27:34 +0200

    chore: add listTileTheme and switchTheme

--- packages/app_ui/test/theme/app_theme_test.dart
+++ packages/app_ui/test/theme/app_theme_test.dart
@@ -1,4 +1,5 @@
 import 'package:app_ui/app_ui.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -16,6 +17,48 @@ void main() {
           const AppTheme().themeData.colorScheme.secondary,
           AppColors.lightBlue.shade300,
         );
+      });
+
+      group('switchTheme', () {
+        group('thumbColor', () {
+          test('returns darkAqua when selected', () {
+            expect(
+              const AppTheme()
+                  .themeData
+                  .switchTheme
+                  .thumbColor
+                  ?.resolve({MaterialState.selected}),
+              equals(AppColors.darkAqua),
+            );
+          });
+
+          test('returns eerieBlack when not selected', () {
+            expect(
+              const AppTheme().themeData.switchTheme.thumbColor?.resolve({}),
+              equals(AppColors.eerieBlack),
+            );
+          });
+        });
+
+        group('trackColor', () {
+          test('returns primaryContainer when selected', () {
+            expect(
+              const AppTheme()
+                  .themeData
+                  .switchTheme
+                  .trackColor
+                  ?.resolve({MaterialState.selected}),
+              equals(AppColors.primaryContainer),
+            );
+          });
+
+          test('returns paleSky when not selected', () {
+            expect(
+              const AppTheme().themeData.switchTheme.trackColor?.resolve({}),
+              equals(AppColors.paleSky),
+            );
+          });
+        });
       });
     });
   });
