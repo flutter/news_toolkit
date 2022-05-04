3a1037227a983118cf6b9329e461e11aafa0e54f
Jan Stepien <jasiek.stepien@gmail.com>
2022-05-04 14:31:23 +0200

    barel file + doc

--- api/packages/news_blocks/lib/news_blocks.dart
+++ api/packages/news_blocks/lib/news_blocks.dart
@@ -1,6 +1,7 @@
 library news_blocks;
 
 export 'src/block_action.dart' show BlockAction, BlockActionType;
+export 'src/divider_horizontal_block.dart' show DividerHorizontalBlock;
 export 'src/news_block.dart' show NewsBlock;
 export 'src/section_header_block.dart' show SectionHeaderBlock;
 export 'src/unknown_block.dart' show UnknownBlock;
