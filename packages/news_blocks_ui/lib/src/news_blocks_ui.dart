c5dc5bb915374c63ffbc4aba983a7b66855bebb7
Felix Angelov <felangelov@gmail.com>
2022-05-04 04:38:42 -0500

    feat(news_blocks_ui): add SectionHeader widget (#72)
    * feat(news_blocks_ui): add SectionHeader widget
    
    * ci: disable test optimizations

--- packages/news_blocks_ui/lib/src/news_blocks_ui.dart
+++
@@ -1,7 +0,0 @@
-/// {@template news_blocks_ui}
-/// A Flutter package that contains UI components for news blocks.
-/// {@endtemplate}
-class NewsBlocksUi {
-  /// {@macro news_blocks_ui}
-  const NewsBlocksUi();
-}
