5dff3e98abb7463a08fa859f0f2e15394b396ae3
Ana Polo <anna-91_@hotmail.com>
2022-05-06 18:50:13 +0200

    fix: resolved pipeline conflict

--- test/user_profile/view/user_profile_page_test.dart
+++ test/user_profile/view/user_profile_page_test.dart
@@ -8,7 +8,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:google_news_template/app/app.dart';
 import 'package:google_news_template/user_profile/user_profile.dart';
 import 'package:mockingjay/mockingjay.dart';
-import 'package:mocktail/mocktail.dart';
 import 'package:user_repository/user_repository.dart';
 
 import '../../helpers/helpers.dart';
