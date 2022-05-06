07ae61dc940527aadcb62d1416601b7d54b846db
Jan Stepien <jasiek.stepien@gmail.com>
2022-05-06 15:08:35 +0200

    post premium final

--- packages/news_blocks_ui/test/src/post_large_test.dart
+++ packages/news_blocks_ui/test/src/post_large_test.dart
@@ -10,16 +10,25 @@ import 'package:news_blocks_ui/news_blocks_ui.dart';
 
 void main() {
   setUpAll(() => HttpOverrides.global = null);
+
+  const id = '499305f6-5096-4051-afda-824dcfc7df23';
+  const category = PostCategory.technology;
+  const author = 'Sean Hollister';
+  final publishedAt = DateTime(2022, 3, 9);
+  const imageUrl =
+      'https://cdn.vox-cdn.com/thumbor/OTpmptgr7XcTVAJ27UBvIxl0vrg='
+      '/0x146:2040x1214/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset'
+      '/file/22049166/shollister_201117_4303_0003.0.jpg';
+  const title = 'Nvidia and AMD GPUs are returning to shelves '
+      'and prices are finally falling';
   group('PostLarge', () {
     final _technologyPostLarge = PostLargeBlock(
-      id: '499305f6-5096-4051-afda-824dcfc7df23',
-      category: PostCategory.technology,
-      author: 'Sean Hollister',
-      publishedAt: DateTime(2022, 3, 9),
-      imageUrl:
-          'https://cdn.vox-cdn.com/thumbor/OTpmptgr7XcTVAJ27UBvIxl0vrg=/0x146:2040x1214/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/22049166/shollister_201117_4303_0003.0.jpg',
-      title: 'Nvidia and AMD GPUs are returning to shelves '
-          'and prices are finally falling',
+      id: id,
+      category: category,
+      author: author,
+      publishedAt: publishedAt,
+      imageUrl: imageUrl,
+      title: title,
     );
 
     testWidgets('renders correctly non-premium', (tester) async {
@@ -44,8 +53,14 @@ void main() {
     });
 
     testWidgets('renders correctly premium', (tester) async {
-      final premiumBlock = PostLargeBlock.fromJson(
-        _technologyPostLarge.toJson()..['isPremium'] = true,
+      final premiumBlock = PostLargeBlock(
+        id: id,
+        category: category,
+        author: author,
+        publishedAt: publishedAt,
+        imageUrl: imageUrl,
+        title: title,
+        isPremium: true,
       );
 
       final widget = MaterialApp(
