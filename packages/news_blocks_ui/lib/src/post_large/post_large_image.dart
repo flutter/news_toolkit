526f98a8f6c7f5f98fc6bd54301cf1d6e8b23b29
Jan Stepien <jasiek.stepien@gmail.com>
2022-05-10 15:33:40 +0200

    remove opacity 0

--- packages/news_blocks_ui/lib/src/post_large/post_large_image.dart
+++ packages/news_blocks_ui/lib/src/post_large/post_large_image.dart
@@ -39,7 +39,7 @@ class PostLargeImage extends StatelessWidget {
                       begin: Alignment.topCenter,
                       end: Alignment.bottomCenter,
                       colors: [
-                        AppColors.black.withOpacity(0),
+                        AppColors.black,
                         AppColors.black.withOpacity(0.7),
                       ],
                     ),
