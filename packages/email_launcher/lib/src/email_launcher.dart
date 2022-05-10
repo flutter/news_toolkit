5c8b2757e3ca53ef30d712620a78762c87e7db4e
Ana Polo <anna-91_@hotmail.com>
2022-05-10 13:27:20 +0200

    fix: resolved pr comments

--- packages/email_launcher/lib/src/email_launcher.dart
+++ packages/email_launcher/lib/src/email_launcher.dart
@@ -2,19 +2,66 @@ import 'package:android_intent_plus/android_intent.dart';
 import 'package:flutter/foundation.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-/// This method checks between platforms to use `Intent` on `Android`
-/// or launchUrl with the scheme `message` on iOS.
-Future<void> launchEmail() async {
-  if (defaultTargetPlatform == TargetPlatform.android) {
-    const intent = AndroidIntent(
-      action: 'android.intent.action.MAIN',
-      category: 'android.intent.category.APP_EMAIL',
-    );
-    await intent.launch();
-  } else if (defaultTargetPlatform == TargetPlatform.iOS) {
-    final url = Uri(scheme: 'message');
-    if (await canLaunchUrl(url)) {
-      await launchUrl(url);
+/// {@template email_launcher_exception}
+/// Exceptions from the email launcher.
+/// {@endtemplate}
+abstract class EmailLauncherException implements Exception {
+  /// {@macro email_launcher_exception}
+  const EmailLauncherException(this.error, this.stackTrace);
+
+  /// The error which was caught.
+  final Object error;
+
+  /// The stack trace associated with the [error].
+  final StackTrace stackTrace;
+}
+
+/// {@template launch_email_app_failure}
+/// Thrown during the launching email app process if a failure occurs.
+/// {@endtemplate}
+class LaunchEmailAppFailure extends EmailLauncherException {
+  /// {@macro send_login_email_link_failure}
+  const LaunchEmailAppFailure(Object error, StackTrace stackTrace)
+      : super(error, stackTrace);
+}
+
+/// Provider to inject `launchUrl`
+typedef LaunchUrlProvider = Future<bool> Function(Uri url);
+
+/// Provider to inject `canLaunchUrl`
+typedef CanLaunchUrlProvider = Future<bool> Function(Uri url);
+
+/// {@template email_launcher}
+/// Class which manage the email launcher logic.
+/// {@endtemplate}
+class EmailLauncher {
+  /// {@macro email_launcher}
+  EmailLauncher({
+    LaunchUrlProvider? launchUrlProvider,
+    CanLaunchUrlProvider? canLaunchProvider,
+  })  : _launchUrlProvider = launchUrlProvider ?? launchUrl,
+        _canLaunchUrlProvider = canLaunchProvider ?? canLaunchUrl;
+
+  final LaunchUrlProvider _launchUrlProvider;
+  final CanLaunchUrlProvider _canLaunchUrlProvider;
+
+  /// Launches a default email app on `Android` and `iOS`.
+  Future<void> launchEmailApp() async {
+    try {
+      if (defaultTargetPlatform == TargetPlatform.android) {
+        const intent = AndroidIntent(
+          action: 'android.intent.action.MAIN',
+          category: 'android.intent.category.APP_EMAIL',
+        );
+        await intent.launch();
+      } else if (defaultTargetPlatform == TargetPlatform.iOS) {
+        final url = Uri(scheme: 'message');
+        if (await _canLaunchUrlProvider(url)) {
+          await _launchUrlProvider(url);
+        }
+      }
+    } catch (error, stackTrace) {
+      throw LaunchEmailAppFailure(error, stackTrace);
     }
   }
 }
--- packages/email_launcher/test/src/email_launcher_test.dart
+++ packages/email_launcher/test/src/email_launcher_test.dart
@@ -1,14 +1,17 @@
 // ignore_for_file: prefer_const_constructors
+
 import 'package:email_launcher/email_launcher.dart';
 import 'package:flutter/foundation.dart';
-import 'package:test/test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher_platform_interface/url_launcher_platform_interface.dart';
 
-import '../../../../test/helpers/mock_url_launcher.dart';
+import '../helpers/helpers.dart';
 
 void main() {
   final mock = MockUrlLauncher();
+  final emailLauncher = EmailLauncher();
 
   setUp(() {
     UrlLauncherPlatform.instance = mock;
@@ -19,43 +22,65 @@ void main() {
       test('is Android', () {
         debugDefaultTargetPlatformOverride = TargetPlatform.android;
         expect(defaultTargetPlatform, TargetPlatform.android);
-        expect(launchEmail(), completes);
+        expect(emailLauncher.launchEmailApp(), completes);
         debugDefaultTargetPlatformOverride = null;
       });
 
-      test('is iOS', () {
-        debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
-        expect(defaultTargetPlatform, TargetPlatform.iOS);
-        debugDefaultTargetPlatformOverride = null;
+      group('when platform is iOS', () {
+        test('completes', () {
+          final emailLauncher = EmailLauncher(
+            launchUrlProvider: (_) => Future.value(true),
+            canLaunchProvider: (_) => Future.value(true),
+          );
+          debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+          expect(defaultTargetPlatform, TargetPlatform.iOS);
+          expect(emailLauncher.launchEmailApp(), completes);
+          WidgetsFlutterBinding.ensureInitialized();
+          debugDefaultTargetPlatformOverride = null;
+        });
+
+        group('calls canLaunch and launchUrl', () {
+          test('when target platform is iOS', () async {
+            debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+            expect(defaultTargetPlatform, TargetPlatform.iOS);
+            final url = Uri(scheme: 'message');
+            mock
+              ..setCanLaunchUrl(url.toString())
+              ..setResponse(true);
+
+            final result = await canLaunchUrl(url);
+            mock
+              ..setLaunchExpectations(
+                url: url.toString(),
+                useSafariVC: false,
+                useWebView: false,
+                enableJavaScript: true,
+                enableDomStorage: true,
+                universalLinksOnly: false,
+                headers: <String, String>{},
+                webOnlyWindowName: null,
+              )
+              ..setResponse(true);
+            final launch = await launchUrl(url);
+            await emailLauncher.launchEmailApp();
+            expect(result, isTrue);
+            expect(launch, isTrue);
+            debugDefaultTargetPlatformOverride = null;
+          });
+        });
       });
-    });
 
-    group('canLaunch and launchUrl', () {
-      test('when target platform is iOS', () async {
+      test(
+          'throws LaunchEmailAppFailure '
+          'on generic exception', () async {
         debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
-        expect(defaultTargetPlatform, TargetPlatform.iOS);
-        final url = Uri(scheme: 'message');
-        mock
-          ..setCanLaunchUrl(url.toString())
-          ..setResponse(true);
-
-        final result = await canLaunchUrl(url);
-        mock
-          ..setLaunchExpectations(
-            url: url.toString(),
-            useSafariVC: false,
-            useWebView: false,
-            enableJavaScript: true,
-            enableDomStorage: true,
-            universalLinksOnly: false,
-            headers: <String, String>{},
-            webOnlyWindowName: null,
-          )
-          ..setResponse(true);
-        final launch = await launchUrl(url);
-        await launchEmail();
-        expect(result, isTrue);
-        expect(launch, isTrue);
+        final emailLauncher = EmailLauncher(
+          launchUrlProvider: (_) => throw Exception(),
+        );
+        expect(
+          emailLauncher.launchEmailApp,
+          throwsA(isA<LaunchEmailAppFailure>()),
+        );
         debugDefaultTargetPlatformOverride = null;
       });
     });
