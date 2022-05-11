e75305d6f44f3ead758959dbf84fa17110ffc8ca
Ana Polo <anna-91_@hotmail.com>
2022-05-11 13:10:04 +0200

    feat(text_paragraph_block_ui): add text paragraph ui (#122)

--- packages/news_blocks_ui/lib/news_blocks_ui.dart
+++ packages/news_blocks_ui/lib/news_blocks_ui.dart
@@ -6,4 +6,5 @@ export 'src/post_small.dart';
 export 'src/section_header.dart';
 export 'src/spacer.dart';
 export 'src/text_headline.dart';
+export 'src/text_paragraph.dart';
 export 'src/widgets/widgets.dart';
