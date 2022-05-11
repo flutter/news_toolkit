4da3f5665187f0c7db947774408c1760c31038c5
Bart Selwesiuk <bartosz@selwesiuk.dev>
2022-05-11 17:50:34 +0300

    chore(feed): add FeedBloc (#115)
    * feat: add FeedBloc
    
    * feat: support feed for multiple categories
    
    * Update lib/feed/bloc/feed_state.dart
    
    * Update test/feed/bloc/feed_bloc_test.dart
    
    * Update test/feed/bloc/feed_bloc_test.dart
    
    * Update test/feed/bloc/feed_bloc_test.dart
    
    Co-authored-by: Bart Selwesiuk <bartosz@verygood.ventures>

---
+++ lib/feed/bloc/feed_event.dart
@@ -0,0 +1,16 @@
+part of 'feed_bloc.dart';
+
+abstract class FeedEvent extends Equatable {
+  const FeedEvent();
+}
+
+class FeedRequested extends FeedEvent {
+  const FeedRequested({
+    required this.category,
+  });
+
+  final Category category;
+
+  @override
+  List<Object> get props => [category];
+}
