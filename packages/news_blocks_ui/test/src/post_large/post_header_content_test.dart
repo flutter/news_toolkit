e34c74b399f32783823785334766f9160bbb5db0
Jan-Stepien <jasiek.stepien@gmail.com>
2022-05-11 15:15:34 +0200

    feat(news_blocks_ui): add PostLarge block ui (#92)
    * add secondary color
    
    * add dateTime extension
    
    * post large implementation
    
    * unit tests
    
    * PR review fixes
    
    * fix goldens
    
    * premium styling
    
    * post large premium goldens
    
    * post premium final
    
    * Update packages/news_blocks_ui/lib/src/post_large.dart
    
    Co-authored-by: Bart Selwesiuk <bartosz@selwesiuk.dev>
    
    * Update packages/news_blocks_ui/lib/src/widgets/post_title.dart
    
    Co-authored-by: Bart Selwesiuk <bartosz@selwesiuk.dev>
    
    * fix image.network comment
    
    * resolve PR comments
    
    * analyzer problem
    
    * premiums test
    
    * styling changes
    
    * use Footer
    
    * remove test
    
    * Update packages/app_ui/lib/src/colors/app_colors.dart
    
    Co-authored-by: Bart Selwesiuk <bartosz@selwesiuk.dev>
    
    * move post_large to post_large/index.dart
    
    * aspect ratio for all images
    
    * remove opacity 0
    
    * resolve PR comments
    
    * PR comment resolution
    
    * add bottom padding
    
    * update goldens
    
    * update index to be barrel file
    
    * move styling under widgets
    
    * revert home page changes
    
    * post small style fix
    
    * update post small goldens
    
    * add aspect ratio into standalone image
    
    * update golden tests
    
    * revert home page
    
    * goldens tests with network image fails
    
    * up
    
    * update comment
    
    * Update packages/news_blocks_ui/lib/src/post_large/post_content.dart
    
    Co-authored-by: Bart Selwesiuk <bartosz@selwesiuk.dev>

---
+++ packages/news_blocks_ui/test/src/post_large/post_header_content_test.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:news_blocks_ui/news_blocks_ui.dart';
+
+import '../../helpers/helpers.dart';
+
+void main() {
+  group('PostHeaderContent', () {
+    final publishedAt = DateTime(2000, 12, 31);
+    const premiumText = 'Premium';
+    const title = 'Title';
+    const author = 'Author';
+    const description = 'Description';
+
+    testWidgets('renders without category', (tester) async {
+      final testPostHeaderContent = PostContent(
+        publishedAt: publishedAt,
+        premiumText: premiumText,
+        title: title,
+        author: author,
+        description: description,
+      );
+
+      await tester.pumpContentThemedApp(testPostHeaderContent);
+
+      expect(find.byType(PostContentCategory), findsNothing);
+    });
+
+    testWidgets('calls onShare when clicked on share icon', (tester) async {
+      final completer = Completer<void>();
+      final testPostHeaderContent = PostContent(
+        publishedAt: publishedAt,
+        premiumText: premiumText,
+        title: title,
+        author: author,
+        description: description,
+        categoryName: 'Category',
+        onShare: completer.complete,
+      );
+
+      await tester.pumpContentThemedApp(testPostHeaderContent);
+
+      await tester.tap(find.byType(IconButton));
+
+      expect(completer.isCompleted, isTrue);
+    });
+  });
+}
