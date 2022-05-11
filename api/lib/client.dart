65ee842e0f102fc084b66045cf638ba99c1d3272
Ana Polo <anna-91_@hotmail.com>
2022-05-11 08:56:39 +0200

    feat(text_paragraph_block): added new text paragraph block

--- api/lib/client.dart
+++ api/lib/client.dart
@@ -15,7 +15,9 @@ export 'package:news_blocks/news_blocks.dart'
         PostGridGroupBlock,
         PostGridTileBlock,
         PostCategory,
-        NewsBlocksConverter;
+        NewsBlocksConverter,
+        TextParagraphBlock;
+
 export 'src/api/v1/categories/get_categories/models/models.dart'
     show CategoriesResponse;
 export 'src/api/v1/feed/get_feed/models/models.dart' show FeedResponse;
