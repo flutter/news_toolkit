7aeae22278e7283d97c07f8d6dbc4db071d4ea33
Ana Polo <anna-91_@hotmail.com>
2022-05-11 13:05:19 +0200

    feat(text_lead_paragraph_ui): add new text lead paragraph ui (#124)

--- packages/news_blocks_ui/lib/news_blocks_ui.dart
+++ packages/news_blocks_ui/lib/news_blocks_ui.dart
@@ -8,5 +8,6 @@ export 'src/post_small.dart';
 export 'src/section_header.dart';
 export 'src/spacer.dart';
 export 'src/text_headline.dart';
+export 'src/text_lead_paragraph.dart';
 export 'src/text_paragraph.dart';
 export 'src/widgets/widgets.dart';
