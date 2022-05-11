210865a22a6bbb0ab64993ecb9bd6310c9cc0c36
Jan Stepien <jasiek.stepien@gmail.com>
2022-05-11 15:43:11 +0200

    doc update

--- packages/news_blocks_ui/lib/src/post_medium/post_medium_overlaid_layout.dart
+++ packages/news_blocks_ui/lib/src/post_medium/post_medium_overlaid_layout.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:news_blocks_ui/news_blocks_ui.dart';
 
 /// {@template post_medium_overlaid_layout}
-/// A reusable post medium news block widget that overlays an image.
+/// A reusable post medium widget that overlays an image.
 /// {@endtemplate}
 class PostMediumOverlaidLayout extends StatelessWidget {
   /// {@macro post_medium_overlaid_layout}
@@ -22,28 +22,25 @@ class PostMediumOverlaidLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
-    return Align(
-      alignment: Alignment.centerRight,
-      child: Stack(
-        alignment: Alignment.bottomLeft,
-        children: [
-          AspectRatio(
-            aspectRatio: 3 / 2,
-            child: OverlaidImage(
-              imageUrl: imageUrl,
-              gradientColor: AppColors.black.withOpacity(0.7),
-            ),
+    return Stack(
+      alignment: Alignment.bottomLeft,
+      children: [
+        AspectRatio(
+          aspectRatio: 3 / 2,
+          child: OverlaidImage(
+            imageUrl: imageUrl,
+            gradientColor: AppColors.black.withOpacity(0.7),
           ),
-          Padding(
-            padding: const EdgeInsets.all(AppSpacing.sm),
-            child: Text(
-              title,
-              style: textTheme.subtitle2
-                  ?.copyWith(color: AppColors.highEmphasisPrimary),
-            ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(AppSpacing.sm),
+          child: Text(
+            title,
+            style: textTheme.subtitle2
+                ?.copyWith(color: AppColors.highEmphasisPrimary),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
