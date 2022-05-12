976782aa239f118218e5168cda3601bb6eafff7f
Ana Polo <anna-91_@hotmail.com>
2022-05-12 17:47:10 +0200

    feat(news_blocks): add text paragraph block (#121)
    * feat(text_paragraph_block): added new text paragraph block
    
    * feat(text_paragraph_block_ui): add text paragraph ui (#122)
    
    * fix: resolve pr comments
    
    * fix: resolve pipeline conflict
    
    Co-authored-by: Bart Selwesiuk <bartosz@verygood.ventures>

--- api/lib/client.dart
+++ api/lib/client.dart
@@ -16,7 +16,8 @@ export 'package:news_blocks/news_blocks.dart'
         PostGridTileBlock,
         PostCategory,
         NewsBlocksConverter,
-        TextHeadlineBlock;
+        TextHeadlineBlock,
+        TextParagraphBlock;
 
 export 'src/api/v1/articles/get_article/models/models.dart'
     show ArticleResponse;
