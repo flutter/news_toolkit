8bc9bbf902e002673e040c6736c05bdfb213ba90
Jan-Stepien <jasiek.stepien@gmail.com>
2022-05-12 10:50:39 +0200

    feat(news_blocks_ui): PostMedium block implementation (#127)
    * add secondary color
    
    * add dateTime extension
    
    * post large implementation
    
    * unit tests
    
    * PR review fixes
    
    * fix goldens
    
    * premium styling
    
    * post large premium goldens
    
    * post premium final
    
    * Update packages/news_blocks_ui/lib/src/post_large.dart
    
    Co-authored-by: Bart Selwesiuk <bartosz@selwesiuk.dev>
    
    * Update packages/news_blocks_ui/lib/src/widgets/post_title.dart
    
    Co-authored-by: Bart Selwesiuk <bartosz@selwesiuk.dev>
    
    * fix image.network comment
    
    * resolve PR comments
    
    * analyzer problem
    
    * premiums test
    
    * styling changes
    
    * use Footer
    
    * remove test
    
    * refactor: extract OverlaidImage from PostLargeImage
    
    * date format
    
    * Update packages/app_ui/lib/src/colors/app_colors.dart
    
    Co-authored-by: Bart Selwesiuk <bartosz@selwesiuk.dev>
    
    * move post_large to post_large/index.dart
    
    * aspect ratio for all images
    
    * remove opacity 0
    
    * resolve PR comments
    
    * PR comment resolution
    
    * add bottom padding
    
    * medium tmp
    
    * update goldens
    
    * update index to be barrel file
    
    * move styling under widgets
    
    * revert home page changes
    
    * post small style fix
    
    * update post small goldens
    
    * add aspect ratio into standalone image
    
    * update golden tests
    
    * revert home page
    
    * goldens tests with network image fails
    
    * up
    
    * tmp
    
    * update comment
    
    * test fix
    
    * revert home page
    
    * post large overlaidImage usage
    
    * remove data format extension
    
    * minor refactor
    
    * doc update
    
    * add horizontal padding
    
    * fix: code review
    
    * Update packages/news_blocks_ui/lib/src/post_medium/post_medium_description_layout.dart
    
    * Update packages/news_blocks_ui/lib/src/post_medium/post_medium_overlaid_layout.dart
    
    * Update packages/news_blocks_ui/lib/src/post_medium/post_medium_overlaid_layout.dart
    
    * Update packages/news_blocks_ui/lib/src/post_medium/post_medium_description_layout.dart
    
    * Update packages/news_blocks_ui/lib/src/post_medium/post_medium_overlaid_layout.dart
    
    * Update packages/news_blocks_ui/lib/src/widgets/overlaid_image.dart
    
    Co-authored-by: Bart Selwesiuk <bartosz@selwesiuk.dev>
    Co-authored-by: Bart Selwesiuk <bartosz@verygood.ventures>

---
+++ packages/news_blocks_ui/lib/src/post_medium/post_medium_description_layout.dart
@@ -0,0 +1,83 @@
+import 'package:app_ui/app_ui.dart';
+import 'package:flutter/material.dart';
+import 'package:news_blocks_ui/news_blocks_ui.dart';
+
+/// {@template post_medium_description_layout}
+/// A reusable post medium news block widget showing post description.
+/// {@endtemplate}
+class PostMediumDescriptionLayout extends StatelessWidget {
+  /// {@macro post_medium_description_layout}
+  const PostMediumDescriptionLayout({
+    Key? key,
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+    required this.publishedAt,
+    this.author,
+    this.onShare,
+  }) : super(key: key);
+
+  /// Title of post.
+  final String title;
+
+  /// Description of post.
+  final String description;
+
+  /// The date when this post was published.
+  final DateTime publishedAt;
+
+  /// The author of this post.
+  final String? author;
+
+  /// Called when the share button is tapped.
+  final VoidCallback? onShare;
+
+  /// The url of this post image.
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: AppSpacing.lg),
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Text(
+                  title,
+                  style: textTheme.headline6
+                      ?.copyWith(color: AppColors.highEmphasisSurface),
+                ),
+              ),
+              const SizedBox(width: AppSpacing.md),
+              Expanded(
+                child: AspectRatio(
+                  aspectRatio: PostMedium.imageAspectRatio,
+                  child: Image.network(
+                    imageUrl,
+                    height: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Text(
+            description,
+            style: textTheme.bodyText2
+                ?.copyWith(color: AppColors.mediumEmphasisSurface),
+          ),
+          const SizedBox(height: AppSpacing.sm),
+          PostFooter(
+            publishedAt: publishedAt,
+            author: author,
+            onShare: onShare,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ packages/news_blocks_ui/lib/src/post_medium/post_medium_overlaid_layout.dart
@@ -0,0 +1,46 @@
+import 'package:app_ui/app_ui.dart';
+import 'package:flutter/material.dart';
+import 'package:news_blocks_ui/news_blocks_ui.dart';
+
+/// {@template post_medium_overlaid_layout}
+/// A reusable post medium widget that overlays the post content on the image.
+/// {@endtemplate}
+class PostMediumOverlaidLayout extends StatelessWidget {
+  /// {@macro post_medium_overlaid_layout}
+  const PostMediumOverlaidLayout({
+    Key? key,
+    required this.title,
+    required this.imageUrl,
+  }) : super(key: key);
+
+  /// Title of post.
+  final String title;
+
+  /// The url of this post image displayed in overlay.
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Stack(
+      alignment: Alignment.bottomLeft,
+      children: [
+        AspectRatio(
+          aspectRatio: PostMedium.imageAspectRatio,
+          child: OverlaidImage(
+            imageUrl: imageUrl,
+            gradientColor: AppColors.black.withOpacity(0.7),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(AppSpacing.sm),
+          child: Text(
+            title,
+            style: textTheme.subtitle2
+                ?.copyWith(color: AppColors.highEmphasisPrimary),
+          ),
+        ),
+      ],
+    );
+  }
+}
