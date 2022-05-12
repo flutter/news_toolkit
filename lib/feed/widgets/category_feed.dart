fdfb59ea25685f28fb70e173bf4f6669cfef9d46
Bart Selwesiuk <bartosz@selwesiuk.dev>
2022-05-12 14:03:25 +0300

    chore(feed): add CategoryFeed widget (#128)
    * feat: add FeedBloc
    
    * feat: support feed for multiple categories
    
    * feat: add CategoryFeed widget
    
    * chore: add missing translation
    
    * Update lib/feed/bloc/feed_state.dart
    
    * Update test/feed/bloc/feed_bloc_test.dart
    
    * Update test/feed/bloc/feed_bloc_test.dart
    
    * Update test/feed/bloc/feed_bloc_test.dart
    
    * feat: add PostMedium and PostLarge to CategoryFeedItem
    
    Co-authored-by: Bart Selwesiuk <bartosz@verygood.ventures>

---
+++ lib/feed/widgets/category_feed.dart
@@ -0,0 +1,77 @@
+import 'package:app_ui/app_ui.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_news_template/feed/feed.dart';
+import 'package:google_news_template/l10n/l10n.dart';
+import 'package:google_news_template_api/client.dart';
+
+class CategoryFeed extends StatelessWidget {
+  const CategoryFeed({
+    Key? key,
+    required this.category,
+  }) : super(key: key);
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    final status = context.select((FeedBloc bloc) => bloc.state.status);
+
+    final categoryFeed =
+        context.select((FeedBloc bloc) => bloc.state.feed[category]) ?? [];
+
+    final hasMoreNews =
+        context.select((FeedBloc bloc) => bloc.state.hasMoreNews[category]) ??
+            false;
+
+    return BlocListener<FeedBloc, FeedState>(
+      listener: (context, state) {
+        if (state.status == FeedStatus.failure) {
+          _handleFailure(context);
+        }
+      },
+      child: status == FeedStatus.initial || status == FeedStatus.loading
+          ? const SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.only(top: AppSpacing.xlg),
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+            )
+          : SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  if (index == categoryFeed.length) {
+                    return hasMoreNews
+                        ? CategoryFeedLoaderItem(
+                            key: ValueKey(index),
+                            onPresented: () => context
+                                .read<FeedBloc>()
+                                .add(FeedRequested(category: category)),
+                          )
+                        : const SizedBox.shrink();
+                  }
+
+                  final block = categoryFeed[index];
+                  return CategoryFeedItem(block: block);
+                },
+                childCount: categoryFeed.length + 1,
+              ),
+            ),
+    );
+  }
+
+  void _handleFailure(BuildContext context) {
+    ScaffoldMessenger.of(context)
+      ..hideCurrentSnackBar()
+      ..showSnackBar(
+        SnackBar(
+          key: const Key('categoryFeed_failure_snackBar'),
+          content: Text(
+            context.l10n.unexpectedFailure,
+          ),
+        ),
+      );
+  }
+}
