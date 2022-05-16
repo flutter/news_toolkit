aa8a7b14b1b8d447979e37a9f054e416b9421582
Felix Angelov <felangelov@gmail.com>
2022-05-16 11:59:39 -0500

    feat(api): add related articles endpoint

--- api/lib/src/api/v1/articles/articles.dart
+++ api/lib/src/api/v1/articles/articles.dart
@@ -1,4 +1,5 @@
 import 'package:google_news_template_api/src/api/v1/articles/get_article/get_article.dart';
+import 'package:google_news_template_api/src/api/v1/articles/get_related_articles/get_related_articles.dart';
 import 'package:google_news_template_api/src/controller.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
@@ -6,12 +7,15 @@ import 'package:shelf_router/shelf_router.dart';
 /// {@template articles}
 /// Controller for `/api/v1/articles` routes.
 /// {@endtemplate}
-class ArticlesController extends Controller with GetArticleMixin {
+class ArticlesController extends Controller
+    with GetArticleMixin, GetRelatedArticlesMixin {
   /// {@macro articles}
   const ArticlesController();
 
   @override
   Handler get handler {
-    return Router()..get('/<id>', getArticle);
+    return Router()
+      ..get('/<id>', getArticle)
+      ..get('/<id>/related', getRelatedArticles);
   }
 }
