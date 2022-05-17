58163903d7fff71c0ad883108c95472a27fafb67
Jan Stepien <jasiek.stepien@gmail.com>
2022-05-17 11:52:53 +0200

    update comments and identifier

--- api/packages/news_blocks/lib/src/article_introduction_block.dart
+++ api/packages/news_blocks/lib/src/article_introduction_block.dart
@@ -3,13 +3,13 @@ import 'package:news_blocks/news_blocks.dart';
 
 part 'article_introduction_block.g.dart';
 
-/// {@template post_large_block}
+/// {@template article_introduction_block}
 /// A block which represents an article introduction block.
 /// https://www.figma.com/file/RajSN6YbRqTuqvdKYtij3b/Google-News-Template-App-v3?node-id=1394%3A51730
 /// {@endtemplate}
 @JsonSerializable()
 class ArticleIntroductionBlock extends PostBlock {
-  /// {@macro post_large_block}
+  /// {@macro article_introduction_block}
   const ArticleIntroductionBlock({
     required super.id,
     required super.category,
@@ -27,7 +27,7 @@ class ArticleIntroductionBlock extends PostBlock {
       _$ArticleIntroductionBlockFromJson(json);
 
   /// The large post block type identifier.
-  static const identifier = '__post_large__';
+  static const identifier = '__article_introduction__';
 
   @override
   Map<String, dynamic> toJson() => _$ArticleIntroductionBlockToJson(this);
