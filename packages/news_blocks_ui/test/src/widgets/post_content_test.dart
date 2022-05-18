270fb5539c6574716227e88c22a71168143381af
Jan Stepien <jasiek.stepien@gmail.com>
2022-05-18 17:32:25 +0200

    update tests

--- packages/news_blocks_ui/test/src/widgets/post_content_test.dart
+++ packages/news_blocks_ui/test/src/widgets/post_content_test.dart
@@ -7,7 +7,7 @@ import '../../helpers/helpers.dart';
 
 void main() {
   group('PostContent', () {
-    testWidgets('renders correctly', (tester) async {
+    testWidgets('renders correctly with title', (tester) async {
       final testPostContent = PostContent(
         title: 'title',
         premiumText: 'premiumText',
@@ -16,9 +16,72 @@ void main() {
       await tester.pumpContentThemedApp(testPostContent);
 
       expect(
-        find.byType(PostFooter),
+        find.text('title'),
         findsOneWidget,
       );
+    });
+
+    testWidgets('renders category', (tester) async {
+      final testPostContent = PostContent(
+        title: 'title',
+        categoryName: 'categoryName',
+        premiumText: 'premiumText',
+      );
+
+      await tester.pumpContentThemedApp(testPostContent);
+
+      expect(
+        find.byType(PostContentCategory),
+        findsOneWidget,
+      );
+    });
+
+    group('renders PostFooter', () {
+      testWidgets('when author provided', (tester) async {
+        final testPostContent = PostContent(
+          title: 'title',
+          author: 'author',
+          premiumText: 'premiumText',
+        );
+
+        await tester.pumpContentThemedApp(testPostContent);
+
+        expect(
+          find.byType(PostFooter),
+          findsOneWidget,
+        );
+      });
+
+      testWidgets('when publishedAt provided', (tester) async {
+        final testPostContent = PostContent(
+          title: 'title',
+          publishedAt: DateTime(2000, 12, 31),
+          premiumText: 'premiumText',
+        );
+
+        await tester.pumpContentThemedApp(testPostContent);
+
+        expect(
+          find.byType(PostFooter),
+          findsOneWidget,
+        );
+      });
+
+      testWidgets('when onShare provided', (tester) async {
+        final testPostContent = PostContent(
+          title: 'title',
+          publishedAt: DateTime(2000, 12, 31),
+          onShare: () {},
+          premiumText: 'premiumText',
+        );
+
+        await tester.pumpContentThemedApp(testPostContent);
+
+        expect(
+          find.byType(PostFooter),
+          findsOneWidget,
+        );
+      });
     });
   });
 }
