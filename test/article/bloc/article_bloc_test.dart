218b0cccbad28aa511019524bcd74ec1523cc1ff
Bart Selwesiuk <bartosz@verygood.ventures>
2022-05-19 11:43:11 +0200

    refactor: export news responses from NewsRepository

--- test/article/bloc/article_bloc_test.dart
+++ test/article/bloc/article_bloc_test.dart
@@ -4,7 +4,6 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:google_news_template/article/article.dart';
-import 'package:google_news_template_api/client.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:news_blocks/news_blocks.dart';
 import 'package:news_repository/news_repository.dart';
