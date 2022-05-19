a1726adc55db9711150811c8a9c1c296a7ceb36f
Jan Stepien <jasiek.stepien@gmail.com>
2022-05-19 15:43:34 +0200

    chore: initialize mobile ads

--- lib/main/bootstrap/bootstrap.dart
+++ lib/main/bootstrap/bootstrap.dart
@@ -5,6 +5,7 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/widgets.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:google_news_template/main/bootstrap/app_bloc_observer.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
@@ -17,6 +18,9 @@ Future<void> bootstrap(Future<Widget> Function() builder) async {
 }
 
 Future<void> _runApp(Future<Widget> Function() builder) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  unawaited(MobileAds.instance.initialize());
+
   await Firebase.initializeApp();
   final blocObserver = AppBlocObserver(
     analyticsRepository: AnalyticsRepository(FirebaseAnalytics()),
