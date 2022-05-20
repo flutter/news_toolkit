87b6958bb68620b37f7539bb24db92de8361ef34
Bart Selwesiuk <bartosz@selwesiuk.dev>
2022-05-20 11:29:43 +0200

    feat(article): add top navigation bar to ArticlePage (#203)
    * feat: add back button to ArticlePage
    
    * test: fix tests
    
    * feat: add article content to static news data
    
    * chore: update cspell.json
    
    * feat(article): add article content to ArticlePage
    
    * refactor: rename key
    
    * revert: main_development.dart changes
    
    * chore: update content
    
    * chore: remove id from ArticleIntroductionBlock
    
    * fix: remove id from ArticleIntroductionBlock
    
    * refactor: export news responses from NewsRepository
    
    * chore(app_ui): add minimum size to small AppButton
    
    * chore(app_ui): add toolbarHeight to AppBarTheme
    
    * feat(article): add top navigation bar to ArticlePage
    
    Co-authored-by: Bart Selwesiuk <bartosz@verygood.ventures>
    Co-authored-by: Jan-Stepien <jasiek.stepien@gmail.com>

--- lib/article/view/article_page.dart
+++ lib/article/view/article_page.dart
@@ -2,6 +2,8 @@ import 'package:app_ui/app_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_news_template/article/article.dart';
+import 'package:google_news_template/l10n/l10n.dart';
+import 'package:news_blocks_ui/news_blocks_ui.dart';
 import 'package:news_repository/news_repository.dart';
 
 class ArticlePage extends StatelessWidget {
@@ -33,8 +35,32 @@ class ArticleView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: const AppBackButton(),
+        title: ShareButton(
+          shareText: context.l10n.shareText,
+          color: AppColors.highEmphasisSurface,
+        ),
+        actions: const [ArticleSubscribeButton()],
       ),
       body: const ArticleContent(),
+    );
+  }
+}
+
+@visibleForTesting
+class ArticleSubscribeButton extends StatelessWidget {
+  const ArticleSubscribeButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: AppSpacing.lg),
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: AppButton.smallRedWine(
+          onPressed: () {},
+          child: Text(context.l10n.subscribeButtonText),
+        ),
+      ),
     );
   }
 }
--- packages/news_blocks_ui/lib/news_blocks_ui.dart
+++ packages/news_blocks_ui/lib/news_blocks_ui.dart
@@ -16,3 +16,4 @@ export 'src/text_headline.dart';
 export 'src/text_lead_paragraph.dart';
 export 'src/text_paragraph.dart';
 export 'src/video.dart';
+export 'src/widgets/widgets.dart' show ShareButton;
