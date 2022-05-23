c313329b62d96f4de7468e3ee75ac2fed4d55ff8
Jan Stepien <jasiek.stepien@gmail.com>
2022-05-23 12:40:28 +0200

    add keywordChagned test

--- test/search/bloc/search_event_test.dart
+++ test/search/bloc/search_event_test.dart
@@ -1,13 +1,26 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:google_news_template/search/search.dart';
 
 void main() {
   group('SearchEvent', () {
-    test('supports value comparisons', () {
-      final event1 = LoadPopular();
-      final event2 = LoadPopular();
+    group('LoadPopular', () {
+      test('supports value comparisons', () {
+        final event1 = LoadPopular();
+        final event2 = LoadPopular();
 
-      expect(event1, equals(event2));
+        expect(event1, equals(event2));
+      });
+    });
+
+    group('KeywordChanged', () {
+      test('supports value comparisons', () {
+        final event1 = KeywordChanged(keyword: 'keyword');
+        final event2 = KeywordChanged(keyword: 'keyword');
+
+        expect(event1, equals(event2));
+      });
     });
   });
 }
