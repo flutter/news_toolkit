e332e5ee8317660a43546dfbd178507e944d4d86
Ana Polo <anna-91_@hotmail.com>
2022-05-23 12:46:58 +0200

    fix: add test

--- test/home/view/home_page_test.dart
+++ test/home/view/home_page_test.dart
@@ -3,11 +3,28 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:google_news_template/feed/feed.dart';
 import 'package:google_news_template/home/home.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:news_repository/news_repository.dart';
 
 import '../../helpers/helpers.dart';
 
+class MockNewsRepository extends Mock implements NewsRepository {}
+
 void main() {
+  late NewsRepository newsRepository;
+
+  setUp(() {
+    newsRepository = MockNewsRepository();
+
+    when(newsRepository.getCategories).thenAnswer(
+      (_) async => CategoriesResponse(
+        categories: [Category.top],
+      ),
+    );
+  });
+
   test('has a page', () {
     expect(HomePage.page(), isA<MaterialPage>());
   });
@@ -15,5 +32,13 @@ void main() {
   testWidgets('renders a HomeView', (tester) async {
     await tester.pumpApp(const HomePage());
     expect(find.byType(HomeView), findsOneWidget);
+  });
+
+  testWidgets('renders FeedView', (tester) async {
+    await tester.pumpApp(
+      HomePage(),
+      newsRepository: newsRepository,
+    );
+    expect(find.byType(FeedView), findsOneWidget);
   });
 }
