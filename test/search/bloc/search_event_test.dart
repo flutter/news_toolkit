2bbae82a024ee0a7fba93a780801309e6cb69cd1
Jan Stepien <jasiek.stepien@gmail.com>
2022-05-27 15:16:12 +0200

    chore(search): implement SearchBloc

---
+++ test/search/bloc/search_event_test.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:google_news_template/search/search.dart';
+
+void main() {
+  group('SearchEvent', () {
+    group('SearchTermChanged', () {
+      test('supports empty value comparisons', () {
+        final event1 = SearchTermChanged();
+        final event2 = SearchTermChanged();
+
+        expect(event1, equals(event2));
+      });
+    });
+
+    test('supports searchTerm value comparisons', () {
+      final event1 = SearchTermChanged(searchTerm: 'keyword');
+      final event2 = SearchTermChanged(searchTerm: 'keyword');
+
+      expect(event1, equals(event2));
+    });
+  });
+}
