f47a0adc133cfaf9614ef155872e4ca14aa9f71e
Jan Stepien <jasiek.stepien@gmail.com>
2022-05-31 13:40:42 +0200

    update barell files

--- lib/notification_preferences/notification_preferences.dart
+++ lib/notification_preferences/notification_preferences.dart
@@ -1,3 +1,3 @@
 export 'bloc/notification_preferences_bloc.dart';
-export 'view//notification_preferences_page.dart';
-export 'widgets/notification_category_tile.dart';
+export 'view/view.dart';
+export 'widgets/widgets.dart';
