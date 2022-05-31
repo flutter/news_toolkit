d9b163cfe27de0e1b3741568c5594f5801472f2a
Jan-Stepien <jasiek.stepien@gmail.com>
2022-05-31 13:07:15 +0200

    feat: implement SearchView (#227)
    * chore(news_repository): add relevantSearch
    
    * chore(search): implement SearchBloc
    
    * add keywordChagned test
    
    * Update test/search/bloc/search_state_test.dart
    
    Co-authored-by: Bart Selwesiuk <bartosz@selwesiuk.dev>
    
    * update topics test
    
    * adding debounce
    
    * fix tests
    
    * chore: remove keyword from SearchBloc
    
    * updated displayMode
    
    * update bloc
    
    * feat: implement SearchView
    
    * implement tests
    
    * fix tests
    
    * resolve PR suggestions
    
    * update naming
    
    * use stream_transform instead
    
    * group tests
    
    * update stream_transform
    
    * fix SearchType tests
    
    * fix tests
    
    * Update lib/l10n/arb/app_en.arb
    
    * Update lib/search/view/search_page.dart
    
    Co-authored-by: Bart Selwesiuk <bartosz@selwesiuk.dev>
    
    * Update lib/l10n/arb/app_en.arb
    
    Co-authored-by: Bart Selwesiuk <bartosz@selwesiuk.dev>
    
    * PR suggestions
    
    * address PR comments
    
    * update keys
    
    * add restartableDebounce transformer
    
    * resolve PR comments
    
    * chore(app_ui): add default chipTheme
    
    * chore(search): implement SearchBloc
    
    * updated tests
    
    * fix PR comments
    
    * update barell file
    
    * update message
    
    Co-authored-by: Bart Selwesiuk <bartosz@selwesiuk.dev>

---
+++ lib/search/view/search_page.dart
@@ -0,0 +1,124 @@
+import 'package:app_ui/app_ui.dart' hide Assets;
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_news_template/feed/feed.dart';
+import 'package:google_news_template/l10n/l10n.dart';
+import 'package:google_news_template/navigation/navigation.dart';
+import 'package:google_news_template/search/search.dart';
+import 'package:google_news_template/user_profile/user_profile.dart';
+import 'package:news_repository/news_repository.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<SearchBloc>(
+      create: (context) => SearchBloc(
+        newsRepository: context.read<NewsRepository>(),
+      )..add(const SearchTermChanged()),
+      child: const SearchView(),
+    );
+  }
+}
+
+class SearchView extends StatefulWidget {
+  const SearchView({super.key});
+
+  @override
+  State<SearchView> createState() => _SearchViewState();
+}
+
+class _SearchViewState extends State<SearchView> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(
+      () => context
+          .read<SearchBloc>()
+          .add(SearchTermChanged(searchTerm: _controller.text)),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return BlocConsumer<SearchBloc, SearchState>(
+      listener: (context, state) {
+        if (state.status == SearchStatus.failure) {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(content: Text(context.l10n.searchErrorMessage)),
+            );
+        }
+      },
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: AppLogo.dark(),
+            centerTitle: true,
+            actions: const [UserProfileButton()],
+          ),
+          drawer: const NavigationDrawer(),
+          body: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SearchTextField(
+                  key: const Key('searchPage_searchTextField'),
+                  controller: _controller,
+                ),
+                const Divider(),
+                SearchHeadlineText(
+                  headerText: state.searchType == SearchType.popular
+                      ? l10n.searchPopularSearches
+                      : l10n.searchRelevantTopics,
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(
+                    AppSpacing.lg,
+                    0,
+                    AppSpacing.lg,
+                    AppSpacing.lg,
+                  ),
+                  child: Wrap(
+                    spacing: AppSpacing.sm,
+                    children: state.topics
+                        .map<Widget>(
+                          (topic) => SearchFilterChip(
+                            key: Key('searchFilterChip_$topic'),
+                            chipText: topic,
+                            onSelected: (text) => _controller.text = text,
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+                const Divider(),
+                SearchHeadlineText(
+                  headerText: state.searchType == SearchType.popular
+                      ? l10n.searchPopularArticles
+                      : l10n.searchRelevantArticles,
+                ),
+                ...state.articles
+                    .map<Widget>(
+                      (newsBlock) => CategoryFeedItem(block: newsBlock),
+                    )
+                    .toList(),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
