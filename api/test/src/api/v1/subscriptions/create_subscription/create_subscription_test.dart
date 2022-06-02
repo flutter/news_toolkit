891a666dcd4bc945d5c5c68fc773bdced9cd3569
Felix Angelov <felangelov@gmail.com>
2022-06-02 00:20:01 -0500

    feat(api): create subscriptions endpoint

---
+++ api/test/src/api/v1/subscriptions/create_subscription/create_subscription_test.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:io';
+
+import 'package:google_news_template_api/src/api/v1/subscriptions/subscriptions.dart';
+import 'package:http/http.dart';
+import 'package:test/test.dart';
+
+import '../../../../../test_server.dart';
+
+void main() {
+  group('POST /api/v1/subscriptions', () {
+    late SubscriptionsController controller;
+
+    setUp(() {
+      controller = SubscriptionsController();
+    });
+
+    testServer(
+      'returns a 201 on success',
+      (host) async {
+        final response = await post(Uri.parse('$host'));
+        expect(response.statusCode, equals(HttpStatus.created));
+      },
+      handler: () => controller.handler,
+    );
+  });
+}
