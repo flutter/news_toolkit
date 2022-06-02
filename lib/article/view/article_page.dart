562697548ec22634d7302d9363102c8669a193b7
Bart Selwesiuk <bartosz@verygood.ventures>
2022-06-02 16:50:26 +0200

    feat(article): add ArticleThemeOverride to ArticlePage

--- lib/article/view/article_page.dart
+++ lib/article/view/article_page.dart
@@ -1,5 +1,6 @@
 import 'package:app_ui/app_ui.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_news_template/ads/ads.dart';
 import 'package:google_news_template/article/article.dart';
@@ -8,13 +9,28 @@ import 'package:news_blocks_ui/news_blocks_ui.dart';
 import 'package:news_repository/news_repository.dart';
 
 class ArticlePage extends StatelessWidget {
-  const ArticlePage({super.key, required this.id});
+  const ArticlePage({
+    super.key,
+    required this.id,
+    required this.isVideoArticle,
+  });
 
   /// The id of the requested article.
   final String id;
 
-  static Route route({required String id}) =>
-      MaterialPageRoute<void>(builder: (_) => ArticlePage(id: id));
+  /// Whether the requested article is a video article.
+  final bool isVideoArticle;
+
+  static Route route({
+    required String id,
+    bool isVideoArticle = false,
+  }) =>
+      MaterialPageRoute<void>(
+        builder: (_) => ArticlePage(
+          id: id,
+          isVideoArticle: isVideoArticle,
+        ),
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -23,27 +39,51 @@ class ArticlePage extends StatelessWidget {
         articleId: id,
         newsRepository: context.read<NewsRepository>(),
       )..add(ArticleRequested()),
-      child: const ArticleView(),
+      child: ArticleView(
+        isVideoArticle: isVideoArticle,
+      ),
     );
   }
 }
 
 class ArticleView extends StatelessWidget {
-  const ArticleView({super.key});
+  const ArticleView({
+    super.key,
+    required this.isVideoArticle,
+  });
+
+  final bool isVideoArticle;
 
   @override
   Widget build(BuildContext context) {
+    final backgroundColor =
+        isVideoArticle ? AppColors.darkBackground : AppColors.white;
+    final foregroundColor =
+        isVideoArticle ? AppColors.white : AppColors.highEmphasisSurface;
+
     return Scaffold(
+      backgroundColor: backgroundColor,
       appBar: AppBar(
-        leading: const AppBackButton(),
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarIconBrightness:
+              isVideoArticle ? Brightness.light : Brightness.dark,
+          statusBarBrightness:
+              isVideoArticle ? Brightness.dark : Brightness.light,
+        ),
+        leading: isVideoArticle
+            ? const AppBackButton.light()
+            : const AppBackButton(),
         title: ShareButton(
           shareText: context.l10n.shareText,
-          color: AppColors.highEmphasisSurface,
+          color: foregroundColor,
         ),
         actions: const [ArticleSubscribeButton()],
       ),
-      body: const InterstitialAd(
-        child: ArticleContent(),
+      body: InterstitialAd(
+        child: ArticleThemeOverride(
+          isVideoArticle: isVideoArticle,
+          child: const ArticleContent(),
+        ),
       ),
     );
   }
