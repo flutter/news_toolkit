ca30c78eaf31c5efe04bde43558d5bb4397a11bb
Bart Selwesiuk <bartosz@selwesiuk.dev>
2022-06-02 11:09:38 +0200

    feat(categories): add categories to NavigationDrawer (#253)
    Co-authored-by: Bart Selwesiuk <bartosz@verygood.ventures>

--- test/navigation/widgets/navigation_drawer_sections_test.dart
+++ test/navigation/widgets/navigation_drawer_sections_test.dart
@@ -1,22 +1,68 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:app_ui/app_ui.dart';
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:google_news_template/categories/categories.dart';
 import 'package:google_news_template/navigation/navigation.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:news_repository/news_repository.dart';
 
 import '../../helpers/helpers.dart';
 
+class MockCategoriesBloc extends MockBloc<CategoriesEvent, CategoriesState>
+    implements CategoriesBloc {}
+
 void main() {
   group('NavigationDrawerSections', () {
+    late CategoriesBloc categoriesBloc;
+
+    const categories = [Category.top, Category.health];
+    final selectedCategory = categories.first;
+
+    setUp(() {
+      categoriesBloc = MockCategoriesBloc();
+      when(() => categoriesBloc.state).thenReturn(
+        CategoriesState.initial().copyWith(
+          categories: categories,
+          selectedCategory: selectedCategory,
+        ),
+      );
+    });
+
     testWidgets('renders NavigationDrawerSectionsTitle', (tester) async {
-      await tester.pumpApp(NavigationDrawerSections());
+      await tester.pumpApp(
+        BlocProvider.value(
+          value: categoriesBloc,
+          child: NavigationDrawerSections(),
+        ),
+      );
       expect(find.byType(NavigationDrawerSectionsTitle), findsOneWidget);
     });
 
-    testWidgets('renders NavigationDrawerSectionItem', (tester) async {
-      await tester.pumpApp(NavigationDrawerSections());
-      expect(find.byType(NavigationDrawerSectionItem), findsWidgets);
+    testWidgets(
+        'renders NavigationDrawerSectionItem '
+        'for each category', (tester) async {
+      await tester.pumpApp(
+        BlocProvider.value(
+          value: categoriesBloc,
+          child: NavigationDrawerSections(),
+        ),
+      );
+
+      for (final category in categories) {
+        expect(
+          find.byWidgetPredicate(
+            (widget) =>
+                widget is NavigationDrawerSectionItem &&
+                widget.key == ValueKey(category) &&
+                widget.selected == (category == selectedCategory),
+          ),
+          findsOneWidget,
+        );
+      }
     });
 
     group('NavigationDrawerSectionItem', () {
