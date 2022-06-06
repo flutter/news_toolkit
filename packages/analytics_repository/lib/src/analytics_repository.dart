47fd4f19341f758fa837ad897cbe9977d8237fa6
Bart Selwesiuk <bartosz@selwesiuk.dev>
2022-06-06 17:09:41 +0200

    feat(analytics_repository): add AnalyticsFailure (#277)
    * feat(analytics_repository): add AnalyticsFailure
    
    * test: update tests
    
    * test: fix tests
    
    Co-authored-by: Bart Selwesiuk <bartosz@verygood.ventures>

--- packages/analytics_repository/lib/src/analytics_repository.dart
+++ packages/analytics_repository/lib/src/analytics_repository.dart
@@ -1,5 +1,28 @@
 import 'package:analytics_repository/analytics_repository.dart';
+import 'package:equatable/equatable.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+
+/// {@template analytics_failure}
+/// A base failure for the analytics repository failures.
+/// {@endtemplate}
+abstract class AnalyticsFailure with EquatableMixin implements Exception {
+  /// {@macro analytics_failure}
+  const AnalyticsFailure(this.error);
+
+  /// The error which was caught.
+  final Object error;
+
+  @override
+  List<Object> get props => [error];
+}
+
+/// {@template track_event_failure}
+/// Thrown when tracking an event fails.
+/// {@endtemplate}
+class TrackEventFailure extends AnalyticsFailure {
+  /// {@macro track_event_failure}
+  const TrackEventFailure(super.error);
+}
 
 /// {@template analytics_repository}
 /// Repository which manages tracking analytics.
@@ -12,10 +35,14 @@ class AnalyticsRepository {
   final FirebaseAnalytics _analytics;
 
   /// Tracks the provided [AnalyticsEvent].
-  void track(AnalyticsEvent event) {
-    _analytics.logEvent(
-      name: event.name,
-      parameters: event.properties,
-    );
+  Future<void> track(AnalyticsEvent event) async {
+    try {
+      await _analytics.logEvent(
+        name: event.name,
+        parameters: event.properties,
+      );
+    } catch (error, stackTrace) {
+      Error.throwWithStackTrace(TrackEventFailure(error), stackTrace);
+    }
   }
 }
