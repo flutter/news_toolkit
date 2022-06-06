bee24c7d55928251273833b5c6e6e5ddaffe155a
Bart Selwesiuk <bartosz@verygood.ventures>
2022-06-06 12:44:43 +0200

    chore: add TokenStorage to main entries

--- lib/main/main_development.dart
+++ lib/main/main_development.dart
@@ -17,7 +17,9 @@ void main() {
     (firebaseDynamicLinks, firebaseMessaging, sharedPreferences) async {
       final tokenStorage = InMemoryTokenStorage();
 
-      final apiClient = GoogleNewsTemplateApiClient();
+      final apiClient = GoogleNewsTemplateApiClient(
+        tokenStorage: tokenStorage,
+      );
 
       const permissionClient = PermissionClient();
 
