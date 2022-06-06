04b428e2416ea9d56b8fc43787e8d777ec0027f5
Jan Stepien <jasiek.stepien@gmail.com>
2022-06-06 12:47:21 +0200

    chore(share_launcher): add tests

--- packages/share_launcher/lib/src/share_launcher.dart
+++ packages/share_launcher/lib/src/share_launcher.dart
@@ -3,15 +3,11 @@ import 'package:share_plus/share_plus.dart';
 /// {@template share_launcher}
 /// A class allowing opening native share bottom sheet.
 /// {@endtemplate}
-abstract class ShareLauncher {
+class ShareLauncher {
   /// {@macro share_launcher}
 
   /// Method for opening native share bottom sheet.
-  static Future<void> share({
-    required String text,
-    String? url,
-    String? subject,
-  }) async {
-    return Share.share('$text \n\n $url');
+  static Future<void> share({required String text}) async {
+    return Share.share(text);
   }
 }
--- packages/share_launcher/test/src/share_launcher_test.dart
+++ packages/share_launcher/test/src/share_launcher_test.dart
@@ -1,15 +1,19 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:share_launcher/share_launcher.dart';
 
 void main() {
   group('ShareLauncher', () {
-    test('calls share', () async {
+    test('calls share with text', () async {
       var called = false;
 
       TestWidgetsFlutterBinding.ensureInitialized();
-      await ShareLauncher.share(text: 'text', url: 'url')
-          .whenComplete(() => called = true);
+
+      MethodChannel('dev.fluttercommunity.plus/share')
+          .setMockMethodCallHandler((call) async => called = true);
+
+      await ShareLauncher.share(text: 'text');
 
       expect(called, isTrue);
     });
