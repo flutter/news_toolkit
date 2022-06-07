a1d2bca472b0ff5f5abd7bf436e0331d20000ca8
Jan Stepien <jasiek.stepien@gmail.com>
2022-06-07 11:15:58 +0200

    clean tests

--- packages/share_launcher/test/src/share_launcher_test.dart
+++ packages/share_launcher/test/src/share_launcher_test.dart
@@ -1,33 +1,15 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 import 'package:share_launcher/share_launcher.dart';
-import 'package:url_launcher_platform_interface/url_launcher_platform_interface.dart';
+import 'package:share_plus_platform_interface/share_plus_platform_interface.dart';
 
-class MockUrlLauncher extends Mock
+class MockSharePlatform extends Mock
     with MockPlatformInterfaceMixin
-    implements UrlLauncherPlatform {}
+    implements SharePlatform {}
 
 void main() {
-  setUpAll(() {
-    UrlLauncherPlatform.instance = MockUrlLauncher();
-
-    when(
-      () => UrlLauncherPlatform.instance.launch(
-        any(that: isA<String>()),
-        useSafariVC: any(named: 'useSafariVC'),
-        useWebView: any(named: 'useWebView'),
-        enableJavaScript: any(named: 'enableJavaScript'),
-        enableDomStorage: any(named: 'enableDomStorage'),
-        universalLinksOnly: any(named: 'universalLinksOnly'),
-        headers: any(named: 'headers'),
-      ),
-    ).thenAnswer((_) async => true);
-  });
-
   group('ShareFailure', () {
     test('supports value comparison', () {
       final shareFailure1 = ShareFailure('error');
@@ -63,38 +45,15 @@ void main() {
 
     test(
         'calls default ShareProvider with text '
-        'when shareProvider not provided '
-        'on iOS', () async {
-      debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
-      TestWidgetsFlutterBinding.ensureInitialized();
+        'when shareProvider not provided ', () async {
       var called = false;
-
-      MethodChannel('dev.fluttercommunity.plus/share')
-          .setMockMethodCallHandler((_) async => called = true);
+      SharePlatform.instance = MockSharePlatform();
+      when(() => SharePlatform.instance.share(any(that: isA<String>())))
+          .thenAnswer((_) async => called = true);
 
       await ShareLauncher().share(text: 'text');
 
       expect(called, isTrue);
-
-      debugDefaultTargetPlatformOverride = null;
-    });
-
-    test(
-        'calls default ShareProvider with text '
-        'when shareProvider not provided '
-        'on android', () async {
-      debugDefaultTargetPlatformOverride = TargetPlatform.android;
-      TestWidgetsFlutterBinding.ensureInitialized();
-      var called = false;
-
-      MethodChannel('dev.fluttercommunity.plus/share')
-          .setMockMethodCallHandler((_) async => called = true);
-
-      await ShareLauncher().share(text: 'text');
-
-      expect(called, isTrue);
-
-      debugDefaultTargetPlatformOverride = null;
     });
   });
 }
