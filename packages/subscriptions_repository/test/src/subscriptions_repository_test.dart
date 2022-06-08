52361459ce3f2d02dceac69a0a84e2e45a13de2f
Bart Selwesiuk <bartosz@verygood.ventures>
2022-06-08 17:32:39 +0200

    chore(subscriptions_repository): call API on requestSubscriptionPlan

--- packages/subscriptions_repository/test/src/subscriptions_repository_test.dart
+++ packages/subscriptions_repository/test/src/subscriptions_repository_test.dart
@@ -1,24 +1,45 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:google_news_template_api/client.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:subscriptions_repository/subscriptions_repository.dart';
 import 'package:test/test.dart';
 
+class MockGoogleNewsTemplateApiClient extends Mock
+    implements GoogleNewsTemplateApiClient {}
+
 void main() {
   group('SubscriptionsRepository', () {
+    late GoogleNewsTemplateApiClient apiClient;
+
+    setUp(() {
+      apiClient = MockGoogleNewsTemplateApiClient();
+      when(apiClient.createSubscription).thenAnswer((_) async {});
+    });
+
     test('can be instantiated', () {
-      expect(SubscriptionsRepository(), isNotNull);
+      expect(
+        SubscriptionsRepository(apiClient: apiClient),
+        isNotNull,
+      );
     });
 
     test('currentSubscriptionPlan emits none when initialized', () {
       expectLater(
-        SubscriptionsRepository().currentSubscriptionPlan,
+        SubscriptionsRepository(apiClient: apiClient).currentSubscriptionPlan,
         emits(SubscriptionPlan.none),
       );
     });
 
     group('requestSubscriptionPlan', () {
+      test('calls ApiClient.createSubscription', () async {
+        final repository = SubscriptionsRepository(apiClient: apiClient);
+        await repository.requestSubscriptionPlan(SubscriptionPlan.premium);
+        verify(apiClient.createSubscription).called(1);
+      });
+
       test('adds plan to currentSubscriptionPlan stream', () {
-        final repository = SubscriptionsRepository();
+        final repository = SubscriptionsRepository(apiClient: apiClient);
 
         expectLater(
           repository.currentSubscriptionPlan,
@@ -29,6 +50,18 @@ void main() {
         );
 
         repository.requestSubscriptionPlan(SubscriptionPlan.premium);
+      });
+
+      test(
+          'throws a RequestSubscriptionPlanFailure '
+          'when ApiClient.createSubscription fails', () async {
+        when(apiClient.createSubscription).thenThrow(Exception());
+
+        expect(
+          () => SubscriptionsRepository(apiClient: apiClient)
+              .requestSubscriptionPlan(SubscriptionPlan.premium),
+          throwsA(isA<RequestSubscriptionPlanFailure>()),
+        );
       });
     });
 
