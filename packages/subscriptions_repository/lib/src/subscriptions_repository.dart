7fb038ca6f2509543668908007800baec62ec02c
Bart Selwesiuk <bartosz@selwesiuk.dev>
2022-06-08 16:21:34 +0200

    chore(subscriptions_repository): add currentSubscriptionPlan and requestSubscriptionPlan (#288)
    * chore(subscriptions_repository): create SubscriptionsRepository
    
    * chore: update pubspec.yaml
    
    * chore(subscriptions_repository): add currentSubscriptionPlan and requestSubscriptionPlan
    
    Co-authored-by: Bart Selwesiuk <bartosz@verygood.ventures>

--- packages/subscriptions_repository/lib/src/subscriptions_repository.dart
+++ packages/subscriptions_repository/lib/src/subscriptions_repository.dart
@@ -1,7 +1,54 @@
+import 'package:equatable/equatable.dart';
+import 'package:rxdart/rxdart.dart';
+
+/// {@template subscriptions_failure}
+/// A base failure for the subscriptions repository failures.
+/// {@endtemplate}
+abstract class SubscriptionsFailure with EquatableMixin implements Exception {
+  /// {@macro subscriptions_failure}
+  const SubscriptionsFailure(this.error);
+
+  /// The error which was caught.
+  final Object error;
+
+  @override
+  List<Object> get props => [error];
+}
+
+/// {@template request_subscription_plan_failure}
+/// Thrown when requesting a subscription plan fails.
+/// {@endtemplate}
+class RequestSubscriptionPlanFailure extends SubscriptionsFailure {
+  /// {@macro request_subscription_plan_failure}
+  const RequestSubscriptionPlanFailure(super.error);
+}
+
+/// The subscription plan of a user.
+enum SubscriptionPlan {
+  /// No subscription plan.
+  none,
+
+  /// Premium subscription plan.
+  premium
+}
+
 /// {@template subscriptions_repository}
 /// A repository that manages user subscriptions.
 /// {@endtemplate}
 class SubscriptionsRepository {
   /// {@macro subscriptions_repository}
-  const SubscriptionsRepository();
+  SubscriptionsRepository()
+      : _currentSubscriptionPlanSubject =
+            BehaviorSubject.seeded(SubscriptionPlan.none);
+
+  final BehaviorSubject<SubscriptionPlan> _currentSubscriptionPlanSubject;
+
+  /// A stream of the current subscription plan of a user.
+  Stream<SubscriptionPlan> get currentSubscriptionPlan =>
+      _currentSubscriptionPlanSubject;
+
+  /// Requests to set the current subscription plan to [plan].
+  Future<void> requestSubscriptionPlan(SubscriptionPlan plan) async {
+    _currentSubscriptionPlanSubject.add(plan);
+  }
 }
