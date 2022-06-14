6b0fc68e807442ef99abcfb112f92860e0493222
Jan Stepien <jasiek.stepien@gmail.com>
2022-06-14 17:10:52 +0200

    updated PurchaseClient

--- packages/purchase_client/lib/src/purchase_client.dart
+++ packages/purchase_client/lib/src/purchase_client.dart
@@ -1,8 +1,31 @@
 import 'dart:async';
 
-import 'package:google_news_template_api/client.dart';
+import 'package:clock/clock.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
+// ignore: implementation_imports
 import 'package:in_app_purchase_platform_interface/src/in_app_purchase_platform_addition.dart';
+
+/// Extension on PurchaseDetails enabling copyWith.
+extension PurchaseDetailsCopyWith on PurchaseDetails {
+  /// Returns a copy of the current PurchaseDetails with the given parameters.
+  PurchaseDetails copyWith({
+    String? purchaseID,
+    String? productID,
+    PurchaseVerificationData? verificationData,
+    String? transactionDate,
+    PurchaseStatus? status,
+    bool? pendingCompletePurchase,
+  }) {
+    return PurchaseDetails(
+      purchaseID: purchaseID ?? this.purchaseID,
+      productID: productID ?? this.productID,
+      verificationData: verificationData ?? this.verificationData,
+      transactionDate: transactionDate ?? this.transactionDate,
+      status: status ?? this.status,
+    )..pendingCompletePurchase =
+        pendingCompletePurchase ?? this.pendingCompletePurchase;
+  }
+}
 
 /// {@template purchase_client}
 /// A PurchaseClient stubbing InAppPurchase implementation.
@@ -11,11 +34,23 @@ import 'package:in_app_purchase_platform_interface/src/in_app_purchase_platform_
 /// {@endtemplate}
 class PurchaseClient implements InAppPurchase {
   /// {@macro purchase_client}
-  PurchaseClient({
-    required GoogleNewsTemplateApiClient apiClient,
-  }) : _apiClient = apiClient;
+  PurchaseClient();
 
-  final GoogleNewsTemplateApiClient _apiClient;
+  /// List of products defined in stores.
+  ///
+  /// Has to be defined in the underlying payment platform, for example,
+  /// [App Store Connect](https://appstoreconnect.apple.com/) for iOS
+  /// and [Google Play Console](https://play.google.com/) for Android.
+  List<ProductDetails> products = <ProductDetails>[
+    ProductDetails(
+      id: '17e79fca-853a-40e3-b4a7-291a64d3846b',
+      title: 'premium',
+      description: 'premium subscription',
+      price: r'$14.99',
+      rawPrice: 14.99,
+      currencyCode: 'USD',
+    ),
+  ];
 
   @override
   Stream<List<PurchaseDetails>> get purchaseStream => _purchaseStream.stream;
@@ -24,11 +59,11 @@ class PurchaseClient implements InAppPurchase {
       StreamController<List<PurchaseDetails>>.broadcast();
 
   @override
-  Future<bool> buyConsumable(
-      {required PurchaseParam purchaseParam, bool autoConsume = true}) {
-    // TODO: implement buyConsumable
-    throw UnimplementedError();
-  }
+  Future<bool> buyConsumable({
+    required PurchaseParam purchaseParam,
+    bool autoConsume = true,
+  }) =>
+      throw UnimplementedError();
 
   @override
   Future<bool> buyNonConsumable({required PurchaseParam purchaseParam}) async {
@@ -40,41 +75,37 @@ class PurchaseClient implements InAppPurchase {
         source: 'local',
       ),
       status: PurchaseStatus.pending,
-      transactionDate: DateTime.now().millisecondsSinceEpoch.toString(),
+      transactionDate: clock.now().millisecondsSinceEpoch.toString(),
     );
 
     _purchaseStream.add([purchaseDetails]);
     try {
-      await _apiClient.createSubscription(
-        subscription: SubscriptionPlan.premium,
-      );
       _purchaseStream.add([
-        purchaseDetails
-          ..status = PurchaseStatus.purchased
-          ..pendingCompletePurchase = true
+        purchaseDetails.copyWith(
+          status: PurchaseStatus.purchased,
+          pendingCompletePurchase: true,
+        ),
       ]);
 
       return Future.value(true);
     } catch (e) {
-      _purchaseStream.add([purchaseDetails..status = PurchaseStatus.canceled]);
+      _purchaseStream.add([purchaseDetails..status = PurchaseStatus.error]);
       return Future.value(false);
     }
   }
 
   @override
   Future<void> completePurchase(PurchaseDetails purchase) async {
-    _purchaseStream.add([purchase..pendingCompletePurchase = false]);
+    _purchaseStream.add([purchase.copyWith(pendingCompletePurchase: false)]);
   }
 
   @override
   T getPlatformAddition<T extends InAppPurchasePlatformAddition?>() {
-    // TODO: implement getPlatformAddition
     throw UnimplementedError();
   }
 
   @override
   Future<bool> isAvailable() {
-    // isAvailable backend endpoint?
     return Future<bool>.delayed(
       const Duration(milliseconds: 100),
     ).then((_) => true);
@@ -82,13 +113,26 @@ class PurchaseClient implements InAppPurchase {
 
   @override
   Future<ProductDetailsResponse> queryProductDetails(Set<String> identifiers) {
-    // TODO: implement queryProductDetails
-    throw UnimplementedError();
+    final notFoundIdentifiers = identifiers
+        .where(
+          (identifier) => !products.any(
+            (product) => product.id == identifier,
+          ),
+        )
+        .toList();
+
+    return Future.value(
+      ProductDetailsResponse(
+        productDetails: products,
+        notFoundIDs: notFoundIdentifiers,
+      ),
+    );
   }
 
   @override
   Future<void> restorePurchases({String? applicationUserName}) {
-    // TODO: implement restorePurchases
-    throw UnimplementedError();
+    // No purchases are restored
+    // in this stubbed implementation of InAppPurchase.
+    return Future.value();
   }
 }
