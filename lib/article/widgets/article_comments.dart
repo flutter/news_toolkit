221584f2a57d0d31c5177a3768eebf279e653d26
simpson-peter <peter@verygood.ventures>
2022-06-13 20:21:19 -0400

    created blank comments widget

---
+++ lib/article/widgets/article_comments.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart' hide Spacer;
+
+class ArticleComments extends StatelessWidget {
+  const ArticleComments({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 50,
+      color: Colors.red,
+    );
+  }
+}
