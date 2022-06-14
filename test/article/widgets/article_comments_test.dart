223b9e3317dd05de343213f3cf437875f8facbd0
simpson-peter <peter@verygood.ventures>
2022-06-14 10:56:04 -0400

    article comments test

---
+++ test/article/widgets/article_comments_test.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:google_news_template/article/article.dart';
+
+import '../../helpers/helpers.dart';
+
+void main() {
+  group('ArticleComments', () {
+    testWidgets('renders ArticlesComments', (tester) async {
+      await tester.pumpApp(
+        ArticleComments(),
+      );
+
+      expect(
+        find.byType(Text),
+        findsOneWidget,
+      );
+      expect(
+        find.byType(TextField),
+        findsOneWidget,
+      );
+    });
+  });
+}
