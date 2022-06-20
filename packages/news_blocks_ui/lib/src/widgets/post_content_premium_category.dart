bc5934d420eb332eea6b2d506c4572c0a2b655a7
simpson-peter <peter@verygood.ventures>
2022-06-20 10:48:27 -0400

    updated class name in premium category doc

--- packages/news_blocks_ui/lib/src/widgets/post_content_premium_category.dart
+++ packages/news_blocks_ui/lib/src/widgets/post_content_premium_category.dart
@@ -1,11 +1,11 @@
 import 'package:app_ui/app_ui.dart';
 import 'package:flutter/material.dart';
 
-/// {@template post_content_category}
+/// {@template post_content_premium_category}
 /// A widget displaying premium label on a post.
 /// {@endtemplate}
 class PostContentPremiumCategory extends StatelessWidget {
-  /// {@macro post_content_category}
+  /// {@macro post_content_premium_category}
   const PostContentPremiumCategory({
     super.key,
     required this.premiumText,
