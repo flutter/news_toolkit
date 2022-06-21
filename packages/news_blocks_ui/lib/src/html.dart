b30e18906fca79942749ce24e4dad58d98558f27
Felix Angelov <felangelov@gmail.com>
2022-06-21 10:35:01 -0500

    feat(news_blocks_ui): Html widget

---
+++ packages/news_blocks_ui/lib/src/html.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_html/flutter_html.dart' as flutter_html;
+
+import 'package:news_blocks/news_blocks.dart';
+
+/// {@template html}
+/// A reusable html news block widget.
+/// {@endtemplate}
+class Html extends StatelessWidget {
+  /// {@macro html}
+  const Html({super.key, required this.block});
+
+  /// The associated [HtmlBlock] instance.
+  final HtmlBlock block;
+
+  @override
+  Widget build(BuildContext context) {
+    return flutter_html.Html(data: block.content);
+  }
+}
