fcebe0e7446552c4918f67501a7ab57246605f9e
simpson-peter <peter@verygood.ventures>
2022-06-21 17:35:02 -0400

    HTML text render test

---
+++ packages/news_blocks_ui/test/src/html_test.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:news_blocks/news_blocks.dart';
+import 'package:news_blocks_ui/news_blocks_ui.dart';
+
+import '../helpers/helpers.dart';
+
+void main() {
+  group('Html', () {
+    testWidgets('renders HTML text correctly', (tester) async {
+      const block = HtmlBlock(content: '<p>Hello</p>');
+
+      await tester.pumpApp(Html(block: block));
+
+      expect(
+        find.text('Hello'),
+        findsOneWidget,
+      );
+    });
+  });
+}
