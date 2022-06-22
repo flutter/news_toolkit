a919b661af794d836e867be84ea5196676c3e7b5
Jan Stepien <jasiek.stepien@gmail.com>
2022-06-22 15:48:06 +0200

    update mock order

--- test/app/bloc/app_bloc_test.dart
+++ test/app/bloc/app_bloc_test.dart
@@ -33,8 +33,8 @@ void main() {
     setUp(() {
       userRepository = MockUserRepository();
       notificationsRepository = MockNotificationsRepository();
-      analyticsRepository = MockAnalyticsRepository();
       inAppPurchaseRepository = MockInAppPurchaseRepository();
+      analyticsRepository = MockAnalyticsRepository();
 
       when(() => userRepository.user).thenAnswer((_) => Stream.empty());
       when(() => inAppPurchaseRepository.currentSubscriptionPlan)
