fb99cc417cc1577a2381b98e04cd246eb3c05f54
simpson-peter <peter@verygood.ventures>
2022-06-23 14:59:29 -0400

    added <p> widget test

--- packages/news_blocks_ui/test/src/html_test.dart
+++ packages/news_blocks_ui/test/src/html_test.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:app_ui/app_ui.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart' as flutter_html;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:news_blocks/news_blocks.dart';
@@ -21,6 +23,38 @@ void main() {
         ),
         findsOneWidget,
       );
+    });
+
+    group('html style', () {
+      testWidgets('styles <p> tags correctly', (tester) async {
+        const block = HtmlBlock(content: '<p>Test</p>');
+
+        final theme = AppTheme().themeData;
+
+        await tester.pumpApp(
+          MaterialApp(
+            theme: theme,
+            home: Html(block: block),
+          ),
+        );
+
+        expect(
+          find.byWidgetPredicate(
+            (widget) =>
+                widget is flutter_html.Html &&
+                widget.style['p']!.generateTextStyle() ==
+                    flutter_html.Style.fromTextStyle(theme.textTheme.bodyText1!)
+                        .generateTextStyle(),
+          ),
+          findsOneWidget,
+        );
+      });
+      testWidgets('styles <h1> tags correctly', (tester) async {});
+      testWidgets('styles <h2> tags correctly', (tester) async {});
+      testWidgets('styles <h3> tags correctly', (tester) async {});
+      testWidgets('styles <h4> tags correctly', (tester) async {});
+      testWidgets('styles <h5> tags correctly', (tester) async {});
+      testWidgets('styles <h6> tags correctly', (tester) async {});
     });
   });
 }
