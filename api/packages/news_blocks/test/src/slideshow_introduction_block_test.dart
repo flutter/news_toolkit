0cb4a4562a1ebb99128e768e8ac657b9abc3169d
simpson-peter <peter@verygood.ventures>
2022-06-24 15:27:59 -0400

    slideshow intro block test

---
+++ api/packages/news_blocks/test/src/slideshow_introduction_block_test.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:math';
+
+import 'package:news_blocks/news_blocks.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('SlideshowIntroductionBlock', () {
+    test('can be (de)serialized', () {
+      final block = SlideshowIntroductionBlock(
+        title: 'title',
+        coverImageUrl: 'coverImageUrl',
+      );
+
+      expect(
+          SlideshowIntroductionBlock.fromJson(block.toJson()), equals(block));
+    });
+  });
+}
