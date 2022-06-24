0dfb2b4a2b9ddb3d3cffe03a8f04e452023d111b
simpson-peter <peter@verygood.ventures>
2022-06-24 14:28:35 -0400

    slide blocks generated files

---
+++ api/packages/news_blocks/lib/src/slide_block.g.dart
@@ -0,0 +1,43 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: cast_nullable_to_non_nullable, implicit_dynamic_parameter, lines_longer_than_80_chars, prefer_const_constructors, require_trailing_commas
+
+part of 'slide_block.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SlideBlock _$SlideBlockFromJson(Map<String, dynamic> json) => $checkedCreate(
+      'SlideBlock',
+      json,
+      ($checkedConvert) {
+        final val = SlideBlock(
+          caption: $checkedConvert('caption', (v) => v as String?),
+          description: $checkedConvert('description', (v) => v as String?),
+          photoCredit: $checkedConvert('photo_credit', (v) => v as String?),
+          imageUrl: $checkedConvert('image_url', (v) => v as String?),
+        );
+        return val;
+      },
+      fieldKeyMap: const {
+        'photoCredit': 'photo_credit',
+        'imageUrl': 'image_url'
+      },
+    );
+
+Map<String, dynamic> _$SlideBlockToJson(SlideBlock instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('caption', instance.caption);
+  writeNotNull('description', instance.description);
+  writeNotNull('photo_credit', instance.photoCredit);
+  writeNotNull('image_url', instance.imageUrl);
+  return val;
+}
---
+++ api/packages/news_blocks/lib/src/slideshow_introduction_block.g.dart
@@ -0,0 +1,41 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: cast_nullable_to_non_nullable, implicit_dynamic_parameter, lines_longer_than_80_chars, prefer_const_constructors, require_trailing_commas
+
+part of 'slideshow_introduction_block.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SlideshowIntroductionBlock _$SlideshowIntroductionBlockFromJson(
+        Map<String, dynamic> json) =>
+    $checkedCreate(
+      'SlideshowIntroductionBlock',
+      json,
+      ($checkedConvert) {
+        final val = SlideshowIntroductionBlock(
+          title: $checkedConvert('title', (v) => v as String),
+          coverImageUrl:
+              $checkedConvert('cover_image_url', (v) => v as String?),
+        );
+        return val;
+      },
+      fieldKeyMap: const {'coverImageUrl': 'cover_image_url'},
+    );
+
+Map<String, dynamic> _$SlideshowIntroductionBlockToJson(
+    SlideshowIntroductionBlock instance) {
+  final val = <String, dynamic>{
+    'title': instance.title,
+  };
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('cover_image_url', instance.coverImageUrl);
+  return val;
+}
