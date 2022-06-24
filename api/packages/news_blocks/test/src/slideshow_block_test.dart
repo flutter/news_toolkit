a15ee240869e2727de9ce6ef5e21dfa5fe648b73
simpson-peter <peter@verygood.ventures>
2022-06-24 15:30:47 -0400

    slideshow block test

---
+++ api/packages/news_blocks/test/src/slideshow_block_test.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:news_blocks/news_blocks.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('SlideshowBlock', () {
+    test('can be (de)serialized', () {
+      final slide = SlideBlock(imageUrl: 'imageUrl', caption: 'caption');
+      final block = SlideshowBlock(title: 'title', slides: [slide, slide]);
+
+      expect(
+        SlideshowBlock.fromJson(block.toJson()),
+        equals(block),
+      );
+    });
+  });
+}
