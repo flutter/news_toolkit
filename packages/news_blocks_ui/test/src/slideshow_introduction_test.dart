2ba30751e1c944d07b0d984d10c20263246fc967
simpson-peter <peter@verygood.ventures>
2022-06-27 13:34:20 -0700

    cover image test

--- packages/news_blocks_ui/test/src/slideshow_introduction_test.dart
+++ packages/news_blocks_ui/test/src/slideshow_introduction_test.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:mocktail_image_network/mocktail_image_network.dart';
 import 'package:news_blocks/news_blocks.dart';
 import 'package:news_blocks_ui/news_blocks_ui.dart';
@@ -9,12 +10,15 @@ import 'package:news_blocks_ui/src/widgets/widgets.dart';
 
 import '../helpers/helpers.dart';
 
+class MockNavigatetoArticleAction extends Mock
+    implements NavigateToFeedCategoryAction {}
+
 void main() {
   const imageUrl =
       'https://cdn.vox-cdn.com/thumbor/OTpmptgr7XcTVAJ27UBvIxl0vrg='
       '/0x146:2040x1214/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset'
       '/file/22049166/shollister_201117_4303_0003.0.jpg';
-  group('SlideshowIntroduction ', () {
+  group('SlideshowIntroduction', () {
     testWidgets('renders title', (tester) async {
       final block = SlideshowIntroductionBlock(
         title: 'title',
@@ -62,11 +66,66 @@ void main() {
       expect(find.byType(SlideshowCategory), findsOneWidget);
     });
 
-    testWidgets('renders cover image', (tester) async {});
+    testWidgets('renders cover image', (tester) async {
+      final block = SlideshowIntroductionBlock(
+        title: 'title',
+        coverImageUrl: imageUrl,
+      );
+
+      await mockNetworkImages(
+        () async => tester.pumpContentThemedApp(
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                SlideshowIntroduction(
+                  block: block,
+                  slideshowText: 'slideshowText',
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+
+      expect(
+        find.byWidgetPredicate(
+          (widget) => widget is PostLargeImage && widget.imageUrl == imageUrl,
+        ),
+        findsOneWidget,
+      );
+    });
 
     testWidgets(
       'onPressed is called with action when tapped',
-      (tester) async {},
+      (tester) async {
+        // final action = MockNavigatetoArticleAction();
+
+        // final block = SlideshowIntroductionBlock(
+        //   title: 'title',
+        //   coverImageUrl: imageUrl,
+        //   action: action,
+        // );
+
+        // await mockNetworkImages(
+        //   () async => tester.pumpContentThemedApp(
+        //     SingleChildScrollView(
+        //       child: Column(
+        //         children: [
+        //           SlideshowIntroduction(
+        //             block: block,
+        //             slideshowText: 'slideshowText',
+        //           ),
+        //         ],
+        //       ),
+        //     ),
+        //   ),
+        // );
+
+        // await tester.ensureVisible(find.byType(SlideshowIntroduction));
+        // await tester.tap(find.byType(SlideshowIntroduction));
+
+        // verify(() => action).called(1);
+      },
     );
   });
 }
