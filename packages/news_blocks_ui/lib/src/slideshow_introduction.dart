2eab60a3bc14f6d56f987a757e47816fc0301f1d
simpson-peter <peter@verygood.ventures>
2022-06-27 10:03:14 -0700

    Created slideshow intro widget

---
+++ packages/news_blocks_ui/lib/src/slideshow_introduction.dart
@@ -0,0 +1,63 @@
+import 'package:app_ui/app_ui.dart';
+import 'package:flutter/material.dart';
+import 'package:news_blocks/news_blocks.dart';
+import 'package:news_blocks_ui/news_blocks_ui.dart';
+import 'package:news_blocks_ui/src/widgets/slideshow_category.dart';
+
+/// {@template post_large}
+/// A reusable slideshow introduction.
+/// {@endtemplate}
+class SlideshowIntroduction extends StatelessWidget {
+  /// {@macro slideshow_introduction}
+  // TODO(simpson-peter): Add this to barrel
+  const SlideshowIntroduction({
+    super.key,
+    required this.block,
+    required this.slideshowText,
+    this.onPressed,
+  });
+
+  /// The associated [SlideshowIntroductionBlock] instance.
+  final SlideshowIntroductionBlock block;
+
+  /// An optional callback which is invoked when the action is triggered.
+  /// A [Uri] from the associated [BlockAction] is provided to the callback.
+  final BlockActionCallback? onPressed;
+
+  /// Text displayed in the slideshow category widget
+  final String slideshowText;
+
+  @override
+  Widget build(BuildContext context) {
+    const isContentOverlaid = true;
+    final textTheme = Theme.of(context).textTheme;
+    return GestureDetector(
+      // TODO call block extension method
+      onTap: () => onPressed?.call(block.action!),
+      child: Stack(
+        key: const Key('slideshow_introduction_stack'),
+        alignment: Alignment.bottomLeft,
+        children: [
+          PostLargeImage(
+            imageUrl: block.coverImageUrl,
+            isContentOverlaid: isContentOverlaid,
+            isLocked: false,
+          ),
+          Column(
+            children: [
+              SlideshowCategory(slideshowText: slideshowText),
+              Text(
+                block.title,
+                style: textTheme.headline3?.copyWith(
+                  color: AppColors.highEmphasisPrimary,
+                ),
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
