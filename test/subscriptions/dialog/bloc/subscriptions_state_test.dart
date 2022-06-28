46c3387e25a14425cb8e3438238ac5e8c91d2407
Jan-Stepien <jasiek.stepien@gmail.com>
2022-06-28 17:20:20 +0200

    chore(subscriptions): SubscriptionsBloc implementation (#352)
    * chore(purchase_client): overview of PurchaseClient
    
    * chore(in_app_purchse): InAppPurchaseRepository overview
    
    * updated PurchaseClient
    
    * add tests
    
    * add unimplemented method tests
    
    * coverage update
    
    * update ID to match backend
    
    * PR comments resolving
    
    * proper repository implementation
    
    * import repository
    
    * chore: create tests for InAppPurchaseRepository
    
    * refactor: rename Subscription to InAppPurchaseRepository
    
    * update lints
    
    * adding flutter_test dependency
    
    * update coverage tests
    
    * styling changes
    
    * update mock order
    
    * update to use flutter_package
    
    * chore: add subscriptions endpoints
    
    * address PR changes requested
    
    * remove comments
    
    * remove unused tests
    
    * add missing tests
    
    * remove unused code
    
    * add Subscription visibility
    
    * chore(app_ui): add missing MediumHighColors
    
    * update colors
    
    * make PurchaseUpdate equatable
    
    * chore(subscriptions): SubscriptionsBloc implementation
    
    * make PurchaseUpdate equatable
    
    * PR comments
    
    * update pr comments
    
    * update tests
    
    * update repo
    
    * update props
    
    * rename purchaseUpdate name
    
    * fix: code review
    
    * fix: code review
    
    Co-authored-by: Bart Selwesiuk <bartosz@verygood.ventures>

---
+++ test/subscriptions/dialog/bloc/subscriptions_state_test.dart
@@ -0,0 +1,114 @@
+// ignore: lines_longer_than_80_chars
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:google_news_template/subscriptions/subscriptions.dart';
+import 'package:in_app_purchase_repository/in_app_purchase_repository.dart';
+
+void main() {
+  group('SubscriptionsState', () {
+    test('initial has correct status', () {
+      final initialState = SubscriptionsState.initial();
+
+      expect(
+        initialState,
+        equals(
+          SubscriptionsState(
+            subscriptions: [],
+            currentSubscription: SubscriptionPlan.none,
+            purchaseStatus: PurchaseStatus.none,
+          ),
+        ),
+      );
+    });
+
+    test('supports value comparisons', () {
+      expect(
+        SubscriptionsState.initial(),
+        equals(
+          SubscriptionsState.initial(),
+        ),
+      );
+    });
+
+    group('copyWith', () {
+      test(
+          'returns same object '
+          'when no parameters changed', () {
+        expect(
+          SubscriptionsState.initial().copyWith(),
+          equals(SubscriptionsState.initial()),
+        );
+      });
+
+      test(
+          'returns object with updated currentSubscription '
+          'when currentSubscription changed', () {
+        expect(
+          SubscriptionsState.initial()
+              .copyWith(currentSubscription: SubscriptionPlan.premium),
+          equals(
+            SubscriptionsState(
+              subscriptions: [],
+              currentSubscription: SubscriptionPlan.premium,
+              purchaseStatus: PurchaseStatus.none,
+            ),
+          ),
+        );
+      });
+      test(
+          'returns object with updated purchaseStatus '
+          'when status changed', () {
+        expect(
+          SubscriptionsState.initial().copyWith(
+            purchaseStatus: PurchaseStatus.completed,
+          ),
+          equals(
+            SubscriptionsState(
+              subscriptions: [],
+              currentSubscription: SubscriptionPlan.none,
+              purchaseStatus: PurchaseStatus.completed,
+            ),
+          ),
+        );
+      });
+
+      test(
+          'returns object with updated subscriptions '
+          'when subscriptions changed', () {
+        expect(
+          SubscriptionsState.initial().copyWith(
+            subscriptions: [
+              Subscription(
+                benefits: [],
+                cost: SubscriptionCost(
+                  annual: 0,
+                  monthly: 0,
+                ),
+                id: '1',
+                name: SubscriptionPlan.none,
+              ),
+            ],
+          ),
+          equals(
+            SubscriptionsState(
+              subscriptions: [
+                Subscription(
+                  benefits: [],
+                  cost: SubscriptionCost(
+                    annual: 0,
+                    monthly: 0,
+                  ),
+                  id: '1',
+                  name: SubscriptionPlan.none,
+                ),
+              ],
+              currentSubscription: SubscriptionPlan.none,
+              purchaseStatus: PurchaseStatus.none,
+            ),
+          ),
+        );
+      });
+    });
+  });
+}
