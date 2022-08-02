b0c54b60522a54e219062827e7c04040fc169165
simpson-peter <peter@verygood.ventures>
2022-08-02 10:22:59 -0400

    TODO format fix

--- lib/article/bloc/article_event.dart
+++ lib/article/bloc/article_event.dart
@@ -55,7 +55,7 @@ class FacebookShareRequested extends ArticleEvent with AnalyticsEventMixin {
 
   final Uri uri;
 
-  // TODO (simpson-peter): See if a more specific analytic event is needed
+  // TODO(simpson-peter): See if a more specific analytic event is needed
   @override
   AnalyticsEvent get event => SocialShareEvent();
 
