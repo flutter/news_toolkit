006e6a338bc265c2c66624403b2b23c8bfe8582f
simpson-peter <peter@verygood.ventures>
2022-08-02 09:32:49 -0400

    create FB share button

---
+++ packages/news_blocks_ui/lib/src/widgets/facebook_share_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+/// {@template facebook_share_button}
+/// A reusable Facebook share button.
+/// {@endtemplate}
+class FacebookShareButton extends StatelessWidget {
+  /// {@macro facebook_share_button}
+  const FacebookShareButton({super.key, this.onTap});
+
+  /// Called when the Icon has been tapped.
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: const Icon(
+        Icons.facebook_rounded,
+      ),
+    );
+  }
+}
