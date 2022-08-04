f4b61c53f46292ae0026dc428c29a7a6a7f02c1d
Bart Selwesiuk <bselwesiuk@gmail.com>
2022-08-04 15:38:46 +0200

    chore: make inMemoryNewsDataSource a singleton instance

--- api/routes/_middleware.dart
+++ api/routes/_middleware.dart
@@ -2,9 +2,11 @@ import 'package:dart_frog/dart_frog.dart';
 
 import 'package:google_news_template_api/api.dart';
 
+final inMemoryNewsDataSource = InMemoryNewsDataSource();
+
 Handler middleware(Handler handler) {
   return handler
       .use(requestLogger())
       .use(userProvider())
-      .use(provider<NewsDataSource>((_) => InMemoryNewsDataSource()));
+      .use(provider<NewsDataSource>((_) => inMemoryNewsDataSource));
 }
