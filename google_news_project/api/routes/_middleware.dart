1dbabe6f1fcff30a9e852bfaf223b2d795c7d083
Felix Angelov <felangelov@gmail.com>
2022-10-25 16:02:55 -0500

    remove unused declaration

--- google_news_project/api/routes/_middleware.dart
+++ google_news_project/api/routes/_middleware.dart
@@ -2,8 +2,6 @@ import 'package:dart_frog/dart_frog.dart';
 
 import 'package:google_news_template_api/api.dart';
 
-final inMemoryNewsDataSource = InMemoryNewsDataSource();
-
 Handler middleware(Handler handler) {
   return handler
       .use(requestLogger())
