2360080ee7f0914fe5939789316dc56402c29d4f
Felix Angelov <felangelov@gmail.com>
2023-01-19 20:49:26 -0600

    chore: remove duplicate assets (#719)

--- flutter_news_example/lib/subscriptions/widgets/subscribe_with_article_limit_modal.dart
+++ flutter_news_example/lib/subscriptions/widgets/subscribe_with_article_limit_modal.dart
@@ -1,12 +1,11 @@
 import 'package:app_ui/app_ui.dart'
-    show AppButton, AppSpacing, AppColors, showAppModal;
+    show AppButton, AppSpacing, AppColors, Assets, showAppModal;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_news_example/ads/ads.dart';
 import 'package:flutter_news_example/analytics/analytics.dart';
 import 'package:flutter_news_example/app/app.dart';
 import 'package:flutter_news_example/article/article.dart';
-import 'package:flutter_news_example/generated/generated.dart';
 import 'package:flutter_news_example/l10n/l10n.dart';
 import 'package:flutter_news_example/login/login.dart';
 import 'package:flutter_news_example/subscriptions/subscriptions.dart';
