53390a17b7c439d384d1f8704681ba2cae3911ef
Bettina Carrizo <betticarrizo@gmail.com>
2023-01-20 16:13:12 -0300

    Feat: Hide Share Button using DartFrog API - Add comment

--- flutter_news_example/api/lib/src/data/in_memory_news_data_source.dart
+++ flutter_news_example/api/lib/src/data/in_memory_news_data_source.dart
@@ -56,7 +56,7 @@ class InMemoryNewsDataSource implements NewsDataSource {
       title: article.title,
       blocks: blocks,
       totalBlocks: totalBlocks,
-      url: Uri(),
+      url: Uri(), // Use article.url to show the share section inside an article
     );
   }
 
