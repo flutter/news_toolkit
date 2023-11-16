ffc9b2db5c06d059788b422a04899f5740c4e9a6
valentinallavayol <vllavayol@gmail.com>
2023-11-16 11:46:52 -0300

    fix: updated models generated files

--- flutter_news_example/api/lib/src/models/subscriptions_response/subscriptions_response.g.dart
+++ flutter_news_example/api/lib/src/models/subscriptions_response/subscriptions_response.g.dart
@@ -1,7 +1,5 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-// ignore_for_file: cast_nullable_to_non_nullable, implicit_dynamic_parameter, lines_longer_than_80_chars, prefer_const_constructors, require_trailing_commas
-
 part of 'subscriptions_response.dart';
 
 // **************************************************************************
@@ -9,24 +7,17 @@ part of 'subscriptions_response.dart';
 // **************************************************************************
 
 SubscriptionsResponse _$SubscriptionsResponseFromJson(
-        Map<String, dynamic> json) =>
-    $checkedCreate(
-      'SubscriptionsResponse',
-      json,
-      ($checkedConvert) {
-        final val = SubscriptionsResponse(
-          subscriptions: $checkedConvert(
-              'subscriptions',
-              (v) => (v as List<dynamic>)
-                  .map((e) => Subscription.fromJson(e as Map<String, dynamic>))
-                  .toList()),
-        );
-        return val;
-      },
+  Map<String, dynamic> json,
+) =>
+    SubscriptionsResponse(
+      subscriptions: (json['subscriptions'] as List<dynamic>)
+          .map((e) => Subscription.fromJson(e as Map<String, dynamic>))
+          .toList(),
     );
 
 Map<String, dynamic> _$SubscriptionsResponseToJson(
-        SubscriptionsResponse instance) =>
+  SubscriptionsResponse instance,
+) =>
     <String, dynamic>{
       'subscriptions': instance.subscriptions.map((e) => e.toJson()).toList(),
     };
