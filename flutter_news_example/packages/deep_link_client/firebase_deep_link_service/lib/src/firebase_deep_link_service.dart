d0afded0a41ed0afeb2b4ba495095e413912f076
Elian Ortega <elian.mortega@gmail.com>
2024-03-28 09:58:11 -0600

    chore: remove late from final variable

--- flutter_news_example/packages/deep_link_client/firebase_deep_link_service/lib/src/firebase_deep_link_service.dart
+++ flutter_news_example/packages/deep_link_client/firebase_deep_link_service/lib/src/firebase_deep_link_service.dart
@@ -3,16 +3,16 @@ import 'dart:async';
 import 'package:deep_link_client/deep_link_client.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 
-/// {@template firebase_deep_link_client}
+/// {@template firebase_deep_link_service}
 /// A FirebaseDynamicLinks implementation of [DeepLinkService].
 /// {@endtemplate}
-class FirebaseDeepLinkService extends DeepLinkService {
-  /// {@macro firebase_deep_link_client}
+class FirebaseDeepLinkService implements DeepLinkService {
+  /// {@macro firebase_deep_link_service}
   FirebaseDeepLinkService({FirebaseDynamicLinks? firebaseDynamicLinks})
       : _firebaseDynamicLinks =
             firebaseDynamicLinks ?? FirebaseDynamicLinks.instance;
 
-  late final FirebaseDynamicLinks _firebaseDynamicLinks;
+  final FirebaseDynamicLinks _firebaseDynamicLinks;
 
   @override
   Stream<Uri> get deepLinkStream =>
